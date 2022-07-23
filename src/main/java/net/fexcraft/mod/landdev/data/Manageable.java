aa6a3395f3c12ab77c6691a0afd1741d4418b666
Ferdinand Calo <fercalo96@yahoo.de>
2022-07-23 23:31:14 +0200

    Create Manageable.java

---
+++ src/main/java/net/fexcraft/mod/landdev/data/Manageable.java
@@ -0,0 +1,45 @@
+package net.fexcraft.mod.landdev.data;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+import net.fexcraft.app.json.JsonArray;
+import net.fexcraft.app.json.JsonMap;
+
+public class Manageable implements Saveable {
+	
+	public UUID manager;
+	public ArrayList<UUID> staff = null;
+	
+	public Manageable(boolean hasstaff){
+		if(hasstaff) staff = new ArrayList<>();
+	}
+
+	@Override
+	public void save(JsonMap map){
+		if(manager != null) map.add("manager", manager.toString());
+		if(staff != null){
+			JsonArray array = map.addArray("staff").asArray();
+			staff.forEach(staff -> array.add(staff.toString()));
+		}
+	}
+
+	@Override
+	public void load(JsonMap map){
+		manager = map.getUUID("manager", null);
+		if(staff != null && map.has("staff")){
+			staff.clear();
+			map.getArray("staff").elements().forEach(elm -> staff.add(UUID.fromString(elm.string_value())));
+		}
+	}
+	
+	public boolean isManager(UUID uuid){
+		return uuid.equals(manager);
+	}
+	
+	public boolean isStaff(UUID uuid){
+		if(staff == null) return false;
+		return staff.contains(uuid);
+	}
+
+}
