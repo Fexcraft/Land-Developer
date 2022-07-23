adb848b40eb2132a40763d8758acc20e4374dea5
Ferdinand Calo <fercalo96@yahoo.de>
2022-07-23 23:31:21 +0200

    Create Norm.java

---
+++ src/main/java/net/fexcraft/mod/landdev/data/norm/Norm.java
@@ -0,0 +1,27 @@
+package net.fexcraft.mod.landdev.data.norm;
+
+import net.fexcraft.app.json.JsonObject;
+
+public abstract class Norm {
+	
+	public final String id;
+	public final NormType type;
+	
+	public Norm(String id, NormType type){
+		this.id = id;
+		this.type = type;
+	}
+	
+	public abstract String string();
+	
+	public abstract int integer();
+	
+	public abstract float decimal();
+	
+	public abstract boolean bool();
+	
+	public abstract JsonObject save();
+	
+	public abstract void load(JsonObject obj);
+
+}
