60205a763b6ca3e2a4c73755d886d4426584747d
Ferdinand Calo <fercalo96@yahoo.de>
2022-07-23 23:31:19 +0200

    Create Norms.java

---
+++ src/main/java/net/fexcraft/mod/landdev/data/Norms.java
@@ -0,0 +1,35 @@
+package net.fexcraft.mod.landdev.data;
+
+import java.util.HashMap;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.landdev.data.norm.Norm;
+
+public class Norms implements Saveable {
+	
+	public HashMap<String, Norm> norms = new HashMap<>();
+	
+	public void add(Norm norm){
+		norms.put(norm.id, norm);
+	}
+
+	@Override
+	public void save(JsonMap map){
+		JsonMap nrms = new JsonMap();
+		norms.entrySet().forEach(entry -> {
+			nrms.add(entry.getKey(), entry.getValue().save());
+		});
+		map.add("norms", nrms);
+	}
+
+	@Override
+	public void load(JsonMap map){
+		if(!map.has("norms")) return;
+		JsonMap nrms = map.getMap("norms");
+		nrms.entries().forEach(entry -> {
+			Norm norm = norms.get(entry.getKey());
+			if(norm != null) norm.load(entry.getValue());
+		});
+	}
+
+}
