130965c05a3e16147d221e4d1e2a3741cd5d924b
Ferdinand Calo <fexcalo@gmail.com>
2022-07-27 23:09:15 +0200

    gui ids

--- src/main/java/net/fexcraft/mod/landdev/gui/GuiHandler.java
+++ src/main/java/net/fexcraft/mod/landdev/gui/GuiHandler.java
@@ -5,6 +5,18 @@ import net.minecraft.world.World;
 import net.minecraftforge.fml.common.network.IGuiHandler;
 
 public class GuiHandler implements IGuiHandler {
+	
+	public static final int PROPERTY = 0;
+	public static final int CHUNK = 1;
+	public static final int COMPANY = 2;
+	public static final int DISTRICT = 3;
+	public static final int MUNICIPALITY = 4;
+	public static final int COUNTY = 5;
+	public static final int STATE = 6;
+	public static final int PLAYER = 8;
+	public static final int POLL = 9;
+	public static final int MAILBOX = 10;
+	
 
 	@Override
 	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z){
