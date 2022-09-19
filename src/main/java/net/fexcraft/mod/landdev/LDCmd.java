ac57dad78a9eee3a1ab71ad95e3bdbdbc9ed7495
Ferdinand Calo <fercalo96@yahoo.de>
2022-09-19 23:27:07 +0200

    Create LDCmd.java

---
+++ src/main/java/net/fexcraft/mod/landdev/LDCmd.java
@@ -0,0 +1,41 @@
+package net.fexcraft.mod.landdev;
+
+import java.util.List;
+
+import net.fexcraft.mod.landdev.util.AliasLoader;
+import net.minecraft.command.CommandBase;
+import net.minecraft.command.ICommandSender;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.server.MinecraftServer;
+
+public class LDCmd extends CommandBase {
+	
+    public LDCmd(){}
+    
+	@Override
+	public String getName(){
+		return AliasLoader.getOverride("landdev");
+	}
+
+	@Override
+	public String getUsage(ICommandSender sender){
+		return "/" + getName();
+	}
+	
+	@Override
+	public List<String> getAliases(){
+		return AliasLoader.getAlias("landdev");
+	}
+    
+    @Override
+    public boolean checkPermission(MinecraftServer server, ICommandSender sender){
+    	return true;
+    }
+
+    @Override 
+    public void execute(MinecraftServer server, ICommandSender sender, String[] args){ 
+    	if(sender instanceof EntityPlayer == false) return;
+    	((EntityPlayer)sender).openGui(LandDev.INSTANCE, -1, sender.getEntityWorld(), 0, 0, 0);
+    }
+
+}
