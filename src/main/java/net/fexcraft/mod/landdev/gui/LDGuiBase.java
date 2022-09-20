deb183f20072b1b4d985c5db5dfbb2827d26a060
Ferdinand Calo <fercalo96@yahoo.de>
2022-09-20 21:05:48 +0200

    Update LDGuiBase.java

--- src/main/java/net/fexcraft/mod/landdev/gui/LDGuiBase.java
+++ src/main/java/net/fexcraft/mod/landdev/gui/LDGuiBase.java
@@ -1,6 +1,9 @@
 package net.fexcraft.mod.landdev.gui;
 
 import static net.fexcraft.mod.landdev.gui.GuiHandler.MAIN;
+import static net.fexcraft.mod.landdev.gui.LDGuiElementType.ELM_GENERIC;
+import static net.fexcraft.mod.landdev.gui.LDGuiElementType.ICON_BLANK;
+import static net.fexcraft.mod.landdev.gui.LDGuiElementType.ICON_OPEN;
 
 import java.util.ArrayList;
 
@@ -26,20 +29,20 @@ public class LDGuiBase extends GenericGui<LDGuiContainer> {
 	
 	@Override
 	public void init(){
-		texts.put("title", new BasicText(guiLeft + 8, guiTop + 8, 196, null, type == MAIN ? "landdev.gui.main.title" : "landdev.gui.loading.title").autoscale().translate());
+		texts.put("title", new BasicText(guiLeft + 8, guiTop + 8, 196, 0x0e0e0e, type == MAIN ? "landdev.gui.main.title" : "landdev.gui.loading.title").autoscale().translate());
 		elements.clear();
 		if(type == MAIN){
-			elements.add(new LDGuiElement("player", LDGuiElementType.ELM_GENERIC).pos(0).text(this, "player"));
-			elements.add(new LDGuiElement("mail", LDGuiElementType.ELM_GENERIC).pos(1).text(this, "mail"));
-			elements.add(new LDGuiElement("prop", LDGuiElementType.ELM_GENERIC).pos(2).text(this, "property"));
-			elements.add(new LDGuiElement("com", LDGuiElementType.ELM_GENERIC).pos(2).text(this, "company"));
-			elements.add(new LDGuiElement("sp0", LDGuiElementType.ELM_EMPTY).pos(4));
-			elements.add(new LDGuiElement("ck", LDGuiElementType.ELM_GENERIC).pos(5).text(this, "chunk"));
-			elements.add(new LDGuiElement("dis", LDGuiElementType.ELM_GENERIC).pos(6).text(this, "district"));
-			elements.add(new LDGuiElement("mun", LDGuiElementType.ELM_GENERIC).pos(7).text(this, "municipality"));
-			elements.add(new LDGuiElement("ct", LDGuiElementType.ELM_GENERIC).pos(8).text(this, "county"));
-			elements.add(new LDGuiElement("st", LDGuiElementType.ELM_GENERIC).pos(9).text(this, "state"));
-			for(int i = 0; i < 9; i++) elements.add(new LDGuiElement("b" + i, LDGuiElementType.ICON_EMPTY).pos(i));
+			int idx = 0;
+			addElm("player", ELM_GENERIC, ICON_OPEN, idx++, "player", () -> {});
+			addElm("mail", ELM_GENERIC, ICON_OPEN, idx++, "mail", null);
+			addElm("prop", ELM_GENERIC, ICON_OPEN, idx++, "property", null);
+			addElm("com", ELM_GENERIC, ICON_OPEN, idx++, "company", null);
+			addElm("spacer", ELM_GENERIC, ICON_BLANK, idx++, null, null);
+			addElm("ck", ELM_GENERIC, ICON_OPEN, idx++, "chunk", null);
+			addElm("dis", ELM_GENERIC, ICON_OPEN, idx++, "district", null);
+			addElm("mun", ELM_GENERIC, ICON_OPEN, idx++, "municipality", null);
+			addElm("ct", ELM_GENERIC, ICON_OPEN, idx++, "county", null);
+			addElm("st", ELM_GENERIC, ICON_OPEN, idx++, "state", null);
 		}
 		else{
 			
@@ -50,6 +53,11 @@ public class LDGuiBase extends GenericGui<LDGuiContainer> {
 		elements.add(new LDGuiElement("test3", LDGuiElementType.ELM_RED).pos(3));
 		elements.add(new LDGuiElement("test4", LDGuiElementType.ELM_EMPTY).pos(4));
 		elements.add(new LDGuiElement("test5", LDGuiElementType.ELM_BLUE).pos(5));*/
+	}
+
+	private void addElm(String id, LDGuiElementType elm, LDGuiElementType icon, int idx, String string, Runnable run){
+		elements.add(new LDGuiElement(id, elm).pos(idx, 0).text(this, string));
+		elements.add(new LDGuiElement(id + "_icon", icon).pos(idx, 1).button(this, run));
 	}
 
 	private void sizeOf(int elms){
@@ -76,6 +84,10 @@ public class LDGuiBase extends GenericGui<LDGuiContainer> {
 
 	public void add(String id, BasicText text){
 		texts.put(id, text);
+	}
+
+	public void add(BasicButton button){
+		buttons.put(button.name, button);
 	}
 
 	public String prefix(){
