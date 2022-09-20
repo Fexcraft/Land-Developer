b610b04d7882a864c4fa9886084786c20eb267bd
Ferdinand Calo <fercalo96@yahoo.de>
2022-09-20 23:25:22 +0200

    Update LDGuiContainer.java

--- src/main/java/net/fexcraft/mod/landdev/gui/LDGuiContainer.java
+++ src/main/java/net/fexcraft/mod/landdev/gui/LDGuiContainer.java
@@ -1,21 +1,16 @@
 package net.fexcraft.mod.landdev.gui;
 
-import static net.fexcraft.mod.landdev.LandDev.INSTANCE;
-import static net.fexcraft.mod.landdev.gui.GuiHandler.*;
-import static net.fexcraft.mod.landdev.gui.LDGuiElementType.ELM_BLANK;
-import static net.fexcraft.mod.landdev.gui.LDGuiElementType.ELM_GENERIC;
-import static net.fexcraft.mod.landdev.gui.LDGuiElementType.ICON_BLANK;
-import static net.fexcraft.mod.landdev.gui.LDGuiElementType.ICON_OPEN;
+import static net.fexcraft.mod.landdev.gui.GuiHandler.MAIN;
 
 import net.fexcraft.lib.mc.gui.GenericContainer;
 import net.fexcraft.lib.mc.gui.GenericGui.BasicText;
 import net.fexcraft.mod.landdev.data.chunk.Chunk_;
+import net.fexcraft.mod.landdev.gui.modules.Main;
 import net.fexcraft.mod.landdev.util.ResManager;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.nbt.NBTBase;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
-import net.minecraft.nbt.NBTTagString;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -43,22 +38,7 @@ public class LDGuiContainer extends GenericContainer {
 		if(packet.getBoolean("sync")){
 			NBTTagCompound com = new NBTTagCompound();
 			switch(type){
-				case MAIN:{
-					com.setString("title_lang", "main.title");
-					NBTTagList list = new NBTTagList();
-					addToList(list, "player", ELM_GENERIC, ICON_OPEN, true, false, null);
-					addToList(list, "mail", ELM_GENERIC, ICON_OPEN, true, false, null);
-					addToList(list, "property", ELM_GENERIC, ICON_OPEN, true, false, null);
-					addToList(list, "company", ELM_GENERIC, ICON_OPEN, true, false, null);
-					addToList(list, "spacer", ELM_BLANK, ICON_BLANK, false, false, null);
-					addToList(list, "chunk", ELM_GENERIC, ICON_OPEN, true, false, null);
-					addToList(list, "district", ELM_GENERIC, ICON_OPEN, true, false, null);
-					addToList(list, "municipality", ELM_GENERIC, ICON_OPEN, true, false, null);
-					addToList(list, "county", ELM_GENERIC, ICON_OPEN, true, false, null);
-					addToList(list, "state", ELM_GENERIC, ICON_OPEN, true, false, null);
-					com.setTag("elements", list);
-					break;
-				}
+				case MAIN:{ Main.INST.sync_packet(this, com); break; }
 			}
 			send(Side.CLIENT, com);
 		}
@@ -66,39 +46,9 @@ public class LDGuiContainer extends GenericContainer {
 			String index = packet.getString("interact");
 			Chunk_ chunk = ResManager.getChunk(player);
 			switch(type){
-				case MAIN:{
-					switch(index){
-						case "player": player.openGui(INSTANCE, PLAYER, player.world, 0, 0, 0); return;
-						case "mail": player.openGui(INSTANCE, MAILBOX, player.world, 0, 0, 0); return;
-						case "property": player.openGui(INSTANCE, PROPERTY, player.world, 0, 0, 0); return;
-						case "company": player.openGui(INSTANCE, COMPANY, player.world, 0, 0, 0); return;
-						case "chunk": player.openGui(INSTANCE, CHUNK, player.world, 0, chunk.key.x, chunk.key.z); return;
-						case "district": player.openGui(INSTANCE, DISTRICT, player.world, 0, chunk.district.id, 0); return;
-						case "municipality":{
-							if(!chunk.district.owner.county)
-								player.openGui(INSTANCE, MUNICIPALITY, player.world, 0, chunk.district.owner.municipality.id, 0);
-							return;
-						}
-						case "county":{
-							player.openGui(INSTANCE, MUNICIPALITY, player.world, 0, chunk.district.owner.county_id(), 0);
-							return;
-						}
-						case "state": player.openGui(INSTANCE, STATE, player.world, 0, 0, 0); return;
-					}
-					break;
-				}
+				case MAIN:{ Main.INST.on_interact(packet, index, player, chunk); break; }
 			}
 		}
-	}
-
-	private void addToList(NBTTagList root, String index, LDGuiElementType elm, LDGuiElementType icon, boolean button, boolean field, String value){
-		NBTTagList list = new NBTTagList();
-		list.appendTag(new NBTTagString(index));
-		list.appendTag(new NBTTagString(elm.name()));
-		list.appendTag(new NBTTagString(icon.name()));
-		list.appendTag(new NBTTagString((elm == ELM_BLANK ? "0" : "1") + (button ? "1" : "0") + (field ? "1" : "0")));
-		if(value != null) list.appendTag(new NBTTagString(value));
-		root.appendTag(list);
 	}
 
 	private void client_packet(NBTTagCompound packet, EntityPlayer player){
