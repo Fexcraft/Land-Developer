1bc09afc09902cb3820db48047c67f62a9f7fdd9
Ferdinand Calo <fercalo96@yahoo.de>
2022-09-21 17:27:54 +0200

    Update Sellable.java

--- src/main/java/net/fexcraft/mod/landdev/data/Sellable.java
+++ src/main/java/net/fexcraft/mod/landdev/data/Sellable.java
@@ -1,6 +1,7 @@
 package net.fexcraft.mod.landdev.data;
 
 import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.fsmm.util.Config;
 import net.fexcraft.mod.landdev.util.Settings;
 
 public class Sellable implements Saveable {
@@ -20,6 +21,10 @@ public class Sellable implements Saveable {
 	@Override
 	public void load(JsonMap map){
 		price = map.getLong("price", layer.is(Layers.CHUNK) ? Settings.DEFAULT_CHUNK_PRICE : 0);
+	}
+
+	public String price_formatted(){
+		return Config.getWorthAsString(price, true);
 	}
 
 }
