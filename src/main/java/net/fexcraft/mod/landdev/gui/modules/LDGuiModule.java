63d908a282d717ca6bd77741855146dadf0ea8b8
Ferdinand Calo <fercalo96@yahoo.de>
2022-09-21 17:28:04 +0200

    Update LDGuiModule.java

--- src/main/java/net/fexcraft/mod/landdev/gui/modules/LDGuiModule.java
+++ src/main/java/net/fexcraft/mod/landdev/gui/modules/LDGuiModule.java
@@ -11,16 +11,18 @@ import net.minecraft.nbt.NBTTagList;
 import net.minecraft.nbt.NBTTagString;
 
 public interface LDGuiModule {
+	
+	public static String LANG_YES = "landdev.gui.yes", LANG_NO = "landdev.gui.no";
 
 	public void sync_packet(LDGuiContainer container, NBTTagCompound com);
 	
-	public default void addToList(NBTTagList root, String index, LDGuiElementType elm, LDGuiElementType icon, boolean button, boolean field, String value){
+	public default void addToList(NBTTagList root, String index, LDGuiElementType elm, LDGuiElementType icon, boolean button, boolean field, Object value){
 		NBTTagList list = new NBTTagList();
 		list.appendTag(new NBTTagString(index));
 		list.appendTag(new NBTTagString(elm.name()));
 		list.appendTag(new NBTTagString(icon.name()));
 		list.appendTag(new NBTTagString((elm == ELM_BLANK ? "0" : "1") + (button ? "1" : "0") + (field ? "1" : "0")));
-		if(value != null) list.appendTag(new NBTTagString(value));
+		if(value != null) list.appendTag(new NBTTagString(value.toString()));
 		root.appendTag(list);
 	}
 
