3a1e9dc643afbf75ae8dd61f71f53b2a470b4b7a
Ferdinand Calo <fercalo96@yahoo.de>
2022-09-25 16:10:23 +0200

    Create PermAction.java

---
+++ src/main/java/net/fexcraft/mod/landdev/data/PermAction.java
@@ -0,0 +1,28 @@
+package net.fexcraft.mod.landdev.data;
+
+public enum PermAction {
+	
+	ACT_CLAIM("claim");
+	
+	public final String norm;
+	
+	PermAction(String normid){
+		norm = normid;
+	}
+
+	public static class PermActions {
+		
+		public final PermAction[] actions;
+		
+		public PermActions(PermAction... actions){
+			this.actions = actions;
+		}
+		
+		public boolean isValid(PermAction act){
+			for(PermAction a : actions) if(act == a) return true;
+			return false;
+		}
+		
+	}
+
+}
