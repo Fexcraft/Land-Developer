d34c8a0e769634d070630bdd064789130ff39b5b
Ferdinand Calo <fercalo96@yahoo.de>
2022-09-29 13:45:59 +0200

    Update LDGuiElement.java

--- src/main/java/net/fexcraft/mod/landdev/gui/LDGuiElement.java
+++ src/main/java/net/fexcraft/mod/landdev/gui/LDGuiElement.java
@@ -2,6 +2,7 @@ package net.fexcraft.mod.landdev.gui;
 
 import net.fexcraft.lib.mc.gui.GenericGui.BasicButton;
 import net.fexcraft.lib.mc.gui.GenericGui.BasicText;
+import net.fexcraft.lib.mc.gui.GenericGui.TextField;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraftforge.fml.relauncher.Side;
@@ -25,7 +26,7 @@ public class LDGuiElement {
 	public LDGuiElement text(LDGuiBase gui, String text, String val){
 		if(text != null || val != null){
 			BasicText telm = new BasicText(gui.getGuiLeft() + 8, gui.getGuiTop() + pos + 3, 196, 0xcdcdcd, "landdev.gui." + (gui.prefix()) + "." + text);
-			gui.add(index, (val == null ? telm.translate() : telm.translate(I18n.format(val))).autoscale());
+			gui.add(index, (val == null ? telm.translate() : telm.translate(I18n.format(val))).hoverable(true).autoscale());
 		}
 		return this;
 	}
@@ -36,6 +37,15 @@ public class LDGuiElement {
 			
 			@Override
 			public boolean onclick(int x, int y, int b){
+				if(type.is_checkbox()){
+					boolean bool = !gui.container().checkboxes.get(index);
+					gui.container().checkboxes.put(index, bool);
+					type = LDGuiElementType.checkbox(bool);
+					tx = type.x;
+					ty = type.y;
+					return true;
+				}
+				if(gui.container().form() && !index.equals("submit")) return true;
 				NBTTagCompound com = new NBTTagCompound();
 				com.setString("interact", index);
 				if(gui.hasField(index)) com.setString("field", gui.getField(index));
@@ -44,6 +54,12 @@ public class LDGuiElement {
 			}
 			
 		});
+		return this;
+	}
+
+	public LDGuiElement field(LDGuiBase gui, String val, boolean wide){
+		gui.add(index, new TextField(pos, gui.fontrenderer(), gui.getGuiLeft() + 7, gui.getGuiTop() + pos + 2, wide ? 212 : 198, 10));
+		if(val != null) gui.setField(index, val);
 		return this;
 	}
 
