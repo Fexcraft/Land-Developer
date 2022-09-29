e916a9eead068f75e4d20839e1a8f1f293d443cf
Ferdinand Calo <fercalo96@yahoo.de>
2022-09-29 13:46:02 +0200

    Update LDGuiElementType.java

--- src/main/java/net/fexcraft/mod/landdev/gui/LDGuiElementType.java
+++ src/main/java/net/fexcraft/mod/landdev/gui/LDGuiElementType.java
@@ -10,6 +10,7 @@ public enum LDGuiElementType {
 	ELM_GENERIC(0, 61, 225, 14),
 	ELM_EMPTY(0, 75, 225, 14),
 	ELM_BLANK(0, 89, 225, 14),
+	ELM_YELLOW(0, 104, 225, 14),
 	ICON_ADD(208, 20, 12, 12),
 	ICON_REM(208, 34, 12, 12),
 	ICON_OPEN(208, 48, 12, 12),
@@ -20,6 +21,8 @@ public enum LDGuiElementType {
 	ICON_EMPTY(208, 118, 12, 12),
 	ICON_ENABLED(208, 132, 12, 12),
 	ICON_DISABLED(208, 146, 12, 12),
+	ICON_CHECKBOX_CHECKED(208, 160, 12, 12),
+	ICON_CHECKBOX_UNCHECKED(208, 174, 12, 12),
 	SCROLLBAR(226, 17, 17, 32),
 	SCROLL_UP(229, 21, 11, 11),
 	SCROLL_DOWN(229, 34, 11, 11),
@@ -36,7 +39,23 @@ public enum LDGuiElementType {
 	}
 
 	boolean icon(){
-		return ordinal() >= 8 && ordinal() < 18;
+		return ordinal() >= 9 && ordinal() < 21;
+	}
+
+	public static LDGuiElementType checkbox(boolean bool){
+		return bool ? ICON_CHECKBOX_CHECKED : ICON_CHECKBOX_UNCHECKED;
+	}
+
+	public boolean is_checkbox(){
+		return this == ICON_CHECKBOX_CHECKED || this == ICON_CHECKBOX_UNCHECKED;
+	}
+
+	public boolean checkbox(){
+		return this == ICON_CHECKBOX_CHECKED;
+	}
+
+	String translation(){
+		return "landdev.gui." + name().toLowerCase();
 	}
 
 }
