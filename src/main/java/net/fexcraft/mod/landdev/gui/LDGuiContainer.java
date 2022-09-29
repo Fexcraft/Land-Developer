43c8207ccbd7f563dc1a81063cb5f7bda1e9219b
Ferdinand Calo <fercalo96@yahoo.de>
2022-09-29 13:45:56 +0200

    Update LDGuiContainer.java

--- src/main/java/net/fexcraft/mod/landdev/gui/LDGuiContainer.java
+++ src/main/java/net/fexcraft/mod/landdev/gui/LDGuiContainer.java
@@ -2,10 +2,15 @@ package net.fexcraft.mod.landdev.gui;
 
 import static net.fexcraft.mod.landdev.gui.GuiHandler.CHUNK;
 import static net.fexcraft.mod.landdev.gui.GuiHandler.MAIN;
+import static net.fexcraft.mod.landdev.gui.GuiHandler.MUNICIPALITY;
+
+import java.util.HashMap;
 
 import net.fexcraft.lib.mc.gui.GenericContainer;
 import net.fexcraft.lib.mc.gui.GenericGui.BasicText;
 import net.fexcraft.mod.landdev.data.chunk.Chunk_;
+import net.fexcraft.mod.landdev.data.municipality.Municipality;
+import net.fexcraft.mod.landdev.data.player.Player;
 import net.fexcraft.mod.landdev.gui.modules.Main;
 import net.fexcraft.mod.landdev.gui.modules.Missing;
 import net.fexcraft.mod.landdev.util.ResManager;
@@ -22,13 +27,18 @@ public class LDGuiContainer extends GenericContainer {
 	public final int type, x, y, z;
 	@SideOnly(Side.CLIENT)
 	public LDGuiBase gui;
+	public Player player;
+	private boolean form;
+	protected HashMap<String, Boolean> checkboxes = new HashMap<>();
 
 	public LDGuiContainer(EntityPlayer player, int id, int x, int y, int z){
 		super(player);
 		switch(type = id){
 			case MAIN: prefix = "main"; break;
 			case CHUNK: prefix = "chunk"; break;
+			case MUNICIPALITY: prefix = "municipality"; break;
 		}
+		this.player = ResManager.getPlayer(player);
 		this.x = x;
 		this.y = y;
 		this.z = z;
@@ -45,8 +55,25 @@ public class LDGuiContainer extends GenericContainer {
 			NBTTagCompound com = new NBTTagCompound();
 			Chunk_ chunk = ResManager.getChunk(player);
 			switch(type){
-				case MAIN: Main.INST.sync_packet(this, com); break;
-				case CHUNK: chunk.sync_packet(this, com); break;
+				case MAIN:{
+					Main.INST.sync_packet(this, com);
+					break;
+				}
+				case CHUNK:{
+					chunk.sync_packet(this, com);
+					break;
+				}
+				case MUNICIPALITY:{
+					if(x < 0){
+						ResManager.getMunicipality(-1, true).sync_packet(this, com);
+						break;
+					}
+					Municipality mun = ResManager.getMunicipality(y, y > -2);
+					if(mun != null){
+						mun.sync_packet(this, com);
+						break;
+					}
+				}
 				default: Missing.INST.sync_packet(this, com); break;
 			}
 			send(Side.CLIENT, com);
@@ -55,8 +82,25 @@ public class LDGuiContainer extends GenericContainer {
 			String index = packet.getString("interact");
 			Chunk_ chunk = ResManager.getChunk(player);
 			switch(type){
-				case MAIN: Main.INST.on_interact(packet, index, player, chunk); break;
-				case CHUNK: chunk.on_interact(packet, index, player, chunk); break;
+				case MAIN:{
+					Main.INST.on_interact(packet, index, player, chunk);
+					break;
+				}
+				case CHUNK:{
+					chunk.on_interact(packet, index, player, chunk);
+					break;
+				}
+				case MUNICIPALITY:{
+					if(x < 0){
+						ResManager.getMunicipality(-1, true).on_interact(packet, index, player, chunk);
+						break;
+					}
+					Municipality mun = ResManager.getMunicipality(y, y > -2);
+					if(mun != null){
+						mun.on_interact(packet, index, player, chunk);
+						break;
+					}
+				}
 				default: Missing.INST.on_interact(packet, index, player, chunk); break;
 			}
 		}
@@ -69,6 +113,7 @@ public class LDGuiContainer extends GenericContainer {
 		gui.sizeOf(list.tagCount());
 		gui.title = new BasicText(gui.getGuiLeft() + 8, gui.getGuiTop() + 8, 196, 0x0e0e0e, "landdev.gui." + packet.getString("title_lang")).autoscale().translate();
 		gui.add("title", gui.title);
+		gui.elements().clear();
 		if(packet.hasKey("title")) gui.title.string = String.format(gui.title.string, packet.getString("title"));
 		int idx = 0;
 		for(NBTBase base : list){
@@ -78,12 +123,20 @@ public class LDGuiContainer extends GenericContainer {
 			LDGuiElementType icon = LDGuiElementType.valueOf(lis.getStringTagAt(2));
 			String bools = lis.getStringTagAt(3);
 			String val = lis.tagCount() > 4 ? lis.getStringTagAt(4) : null;
-			gui.addElm(index, elm, icon, idx++, bools.charAt(0) == '1', bools.charAt(1) == '1', val);
+			gui.addElm(index, elm, icon, idx++, bools.charAt(0) == '1', bools.charAt(1) == '1', bools.charAt(2) == '1', val);
+			if(icon.is_checkbox()){
+				checkboxes.put(index, icon.checkbox());
+			}
 		}
+		if(packet.hasKey("form")) form = packet.getBoolean("form");
 	}
 
-	public EntityPlayer player(){
+	public Player player(){
 		return player;
+	}
+
+	public boolean form(){
+		return form;
 	}
 	
 }
