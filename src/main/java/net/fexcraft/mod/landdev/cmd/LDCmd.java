9cac0a49274f9bb2a90ccf8b5446ee2401df6d63
Ferdinand Calo <fercalo96@yahoo.de>
2022-09-30 22:41:21 +0200

    Update LDCmd.java

--- src/main/java/net/fexcraft/mod/landdev/cmd/LDCmd.java
+++ src/main/java/net/fexcraft/mod/landdev/cmd/LDCmd.java
@@ -2,8 +2,9 @@ package net.fexcraft.mod.landdev.cmd;
 
 import java.util.List;
 
-import net.fexcraft.mod.landdev.LandDev;
+import net.fexcraft.mod.landdev.data.player.Player;
 import net.fexcraft.mod.landdev.util.AliasLoader;
+import net.fexcraft.mod.landdev.util.ResManager;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
@@ -36,7 +37,8 @@ public class LDCmd extends CommandBase {
     @Override 
     public void execute(MinecraftServer server, ICommandSender sender, String[] args){ 
     	if(sender instanceof EntityPlayer == false) return;
-    	((EntityPlayer)sender).openGui(LandDev.INSTANCE, -1, sender.getEntityWorld(), 0, 0, 0);
+    	Player player = ResManager.getPlayer((EntityPlayer)sender);
+    	player.openGui(-1, 0, (int)player.entity.posX >> 4, (int)player.entity.posZ >> 4);
     }
 
 }
