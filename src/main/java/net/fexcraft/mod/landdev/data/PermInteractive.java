864ee906316ba6e417c35616f8c8fc79508e6414
Ferdinand Calo <fercalo96@yahoo.de>
2022-11-03 12:06:56 +0100

    Update PermInteractive.java

--- src/main/java/net/fexcraft/mod/landdev/data/PermInteractive.java
+++ src/main/java/net/fexcraft/mod/landdev/data/PermInteractive.java
@@ -2,10 +2,8 @@ package net.fexcraft.mod.landdev.data;
 
 import java.util.UUID;
 
-import net.minecraft.entity.player.EntityPlayer;
-
 public interface PermInteractive {
 	
-	public boolean can(PermAction act, EntityPlayer player, UUID uuid);
+	public boolean can(PermAction act, UUID uuid);
 
 }
