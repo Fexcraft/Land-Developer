96c32e693c240a1e9fc98584457d94ac8014ede0
Ferdinand Calo <fercalo96@yahoo.de>
2022-11-03 12:06:54 +0100

    Update PermAction.java

--- src/main/java/net/fexcraft/mod/landdev/data/PermAction.java
+++ src/main/java/net/fexcraft/mod/landdev/data/PermAction.java
@@ -3,7 +3,11 @@ package net.fexcraft.mod.landdev.data;
 public enum PermAction {
 	
 	ACT_CLAIM("claim"),
-	ACT_CREATE_LAYER("create_layer");
+	ACT_CREATE_LAYER("create_layer"),
+	ACT_MANAGE_DISTRICT("manage_district"),
+	ACT_SET_CHUNK_TAX("set_tax_chunk"),
+	ACT_SET_PLAYER_TAX("set_tax_player"),
+	;
 	
 	public final String norm;
 	
