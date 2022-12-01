45d704e79e023d217b147a0b0a82f33b9386fd38
Ferdinand Calo <fercalo96@yahoo.de>
2022-12-01 14:10:27 +0100

    Update Municipality.java

--- src/main/java/net/fexcraft/mod/landdev/data/municipality/Municipality.java
+++ src/main/java/net/fexcraft/mod/landdev/data/municipality/Municipality.java
@@ -2,8 +2,9 @@ package net.fexcraft.mod.landdev.data.municipality;
 
 import static net.fexcraft.mod.landdev.data.PermAction.ACT_CLAIM;
 import static net.fexcraft.mod.landdev.data.PermAction.ACT_CREATE_LAYER;
-import static net.fexcraft.mod.landdev.data.PermAction.ACT_SET_CHUNK_TAX;
-import static net.fexcraft.mod.landdev.data.PermAction.ACT_SET_PLAYER_TAX;
+import static net.fexcraft.mod.landdev.data.PermAction.ACT_MANAGE_FINANCES;
+import static net.fexcraft.mod.landdev.data.PermAction.ACT_SET_TAX_CHUNK;
+import static net.fexcraft.mod.landdev.data.PermAction.ACT_SET_TAX_PLAYER;
 import static net.fexcraft.mod.landdev.data.PermAction.ACT_USE_FINANCES;
 import static net.fexcraft.mod.landdev.gui.LDGuiElementType.ELM_BLANK;
 import static net.fexcraft.mod.landdev.gui.LDGuiElementType.ELM_BLUE;
@@ -43,7 +44,7 @@ import net.minecraft.nbt.NBTTagList;
 
 public class Municipality implements Saveable, Layer, LDGuiModule {
 
-	public static PermActions mactions = new PermActions(ACT_CLAIM, ACT_SET_CHUNK_TAX, ACT_SET_PLAYER_TAX, ACT_USE_FINANCES, ACT_USE_FINANCES);
+	public static PermActions mactions = new PermActions(ACT_CLAIM, ACT_SET_TAX_CHUNK, ACT_SET_TAX_PLAYER, ACT_USE_FINANCES, ACT_MANAGE_FINANCES);
 	public static PermActions cactions = new PermActions();
 	public final int id;
 	public Createable created = new Createable();
