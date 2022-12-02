909a53e64a4d33d6f6f98d6b94117f79822e2ac7
Ferdinand Calo <fercalo96@yahoo.de>
2022-12-02 22:45:33 +0100

    Update LDGuiModule.java

--- src/main/java/net/fexcraft/mod/landdev/gui/modules/LDGuiModule.java
+++ src/main/java/net/fexcraft/mod/landdev/gui/modules/LDGuiModule.java
@@ -11,20 +11,37 @@ import net.minecraft.nbt.NBTTagString;
 
 public interface LDGuiModule {
 	
-	public static String LANG_YES = "landdev.gui.yes", LANG_NO = "landdev.gui.no";
+	public static final String LANG_YES = "landdev.gui.yes", LANG_NO = "landdev.gui.no", VALONLY = "!!!";
+	public static final int UI_MAIN = 0;
 
 	public void sync_packet(LDGuiContainer container, NBTTagCompound com);
-	
+
 	public default void addToList(NBTTagList root, String index, LDGuiElementType elm, LDGuiElementType icon, boolean button, boolean field, Object value){
+		addToList(root, index, elm, icon, button, field, value, false);
+	}
+	
+	public default void addToList(NBTTagList root, String index, LDGuiElementType elm, LDGuiElementType icon, boolean button, boolean field, Object value, boolean valonly){
 		NBTTagList list = new NBTTagList();
 		list.appendTag(new NBTTagString(index));
 		list.appendTag(new NBTTagString(elm.name()));
 		list.appendTag(new NBTTagString(icon.name()));
 		list.appendTag(new NBTTagString((elm == ELM_BLANK ? "0" : "1") + (button ? "1" : "0") + (field ? "1" : "0")));
-		if(value != null) list.appendTag(new NBTTagString(value.toString()));
+		if(value != null) list.appendTag(new NBTTagString(valonly ? VALONLY + value.toString() : value.toString()));
 		root.appendTag(list);
 	}
 
 	public void on_interact(LDGuiContainer container, Player player, NBTTagCompound packet, String index);
+	
+	public default boolean validateName(LDGuiContainer container, String name){
+		if(name.length() < 1){
+			container.sendMsg("landdev.cmd.name_too_short", false);
+			return false;
+		}
+		if(name.length() > 32){
+			container.sendMsg("landdev.cmd.name_too_long", false);
+			return false;
+		}
+		return true;
+	}
 	
 }
