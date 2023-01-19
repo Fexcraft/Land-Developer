f91422eceed29300d496a587c29cd1e9990b83bc
Ferdinand Calo <fercalo96@yahoo.de>
2023-01-19 11:33:49 +0100

    Update BroadcastChannel.java

--- src/main/java/net/fexcraft/mod/landdev/util/broad/BroadcastChannel.java
+++ src/main/java/net/fexcraft/mod/landdev/util/broad/BroadcastChannel.java
@@ -2,12 +2,17 @@ package net.fexcraft.mod.landdev.util.broad;
 
 public enum BroadcastChannel {
 	
-	CHAT, ANNOUNCE, PLAYER, STATE, COUNTY, MUNICIPALITY, DISTRICT, COMPANY;
+	CHAT, SERVER, ANNOUNCE, PLAYER, STATE, COUNTY, MUNICIPALITY, DISTRICT, COMPANY;
 	
 	public String name;
 	
 	BroadcastChannel(){
 		name = name().toLowerCase();
+	}
+	
+	@Override
+	public String toString(){
+		return name;
 	}
 
 }
