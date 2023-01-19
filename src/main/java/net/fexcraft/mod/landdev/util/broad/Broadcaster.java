1ab1a38a7095f87df8b937ce4bba38a6c40c7d0c
Ferdinand Calo <fercalo96@yahoo.de>
2023-01-19 11:33:51 +0100

    Update Broadcaster.java

--- src/main/java/net/fexcraft/mod/landdev/util/broad/Broadcaster.java
+++ src/main/java/net/fexcraft/mod/landdev/util/broad/Broadcaster.java
@@ -15,17 +15,26 @@ public class Broadcaster {
 	static { SENDERS.add(new InternalTransmitter()); }
 
 	public static void send(Player player, String message){
-		send(CHAT.name, player.name(), message, player.adm ? Settings.CHAT_ADMIN_COLOR : Settings.CHAT_PLAYER_COLOR);
+		send(CHAT.name, player.name(), message, player.adm ? Settings.CHAT_ADMIN_COLOR : Settings.CHAT_PLAYER_COLOR, false);
 	}
 
-	private static void send(String channel, String sender, String message, String arg){
-		for(Transmitter trs : SENDERS) trs.transmit(channel, sender, message, arg);
+	public static void send(BroadcastChannel channel, String sender, String message, String arg, boolean no_internal){
+		send(channel.name(), sender, message, arg, no_internal);
+	}
+
+	public static void send(String channel, String sender, String message, String arg, boolean no_internal){
+		for(Transmitter trs : SENDERS){
+			if(no_internal && trs.internal()) continue;
+			trs.transmit(channel, sender, message, arg);
+		}
 	}
 
 	public static interface Transmitter {
 		
 		public void transmit(String channel, String sender, String msg, @Nullable String color);
 		
+		public default boolean internal(){ return false; }
+
 		public String category();
 		
 	}
