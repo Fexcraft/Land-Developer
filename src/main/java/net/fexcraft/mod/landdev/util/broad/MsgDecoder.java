80b908ac7f091b65de2eac1f8af67ddd123f3d21
Ferdinand Calo <fercalo96@yahoo.de>
2023-01-19 21:45:43 +0100

    Create MsgDecoder.java

---
+++ src/main/java/net/fexcraft/mod/landdev/util/broad/MsgDecoder.java
@@ -0,0 +1,23 @@
+package net.fexcraft.mod.landdev.util.broad;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ReplayingDecoder;
+
+public class MsgDecoder extends ReplayingDecoder<Message> {
+
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+        Message data = new Message();
+        data.length = in.readInt();
+        if(data.length <= 0){
+        	out.add(data);
+        	return;
+        }
+        data.value = in.readCharSequence(data.length, StandardCharsets.UTF_8).toString();
+        out.add(data);
+    }
+}
