625fffdc141ad997215d3490250b0b529fec7190
Ferdinand Calo <fercalo96@yahoo.de>
2023-01-20 23:01:29 +0100

    Update InternalTransmitter.java

--- src/main/java/net/fexcraft/mod/landdev/util/broad/InternalTransmitter.java
+++ src/main/java/net/fexcraft/mod/landdev/util/broad/InternalTransmitter.java
@@ -20,16 +20,25 @@ import net.minecraft.nbt.NBTTagString;
 public class InternalTransmitter implements Transmitter {
 
 	@Override
-	public void transmit(String channel, String sender, String message, String[] args){
+	public void transmit(String channel, String sender, String message, Object[] args){
 		if(channel.equals(CHAT.name) && !Settings.CHAT_OVERRIDE) return;
 		NBTTagCompound com = new NBTTagCompound();
 		com.setString("target_listener", PacketReceiver.RECEIVER_ID);
 		com.setString("task", "chat_message");
 		NBTTagList list = new NBTTagList();
-		list.appendTag(new NBTTagString(channel));
-		list.appendTag(new NBTTagString(sender));
-		list.appendTag(new NBTTagString(message));
-		if(args != null) list.appendTag(new NBTTagString(args[0]));
+		if(args != null && args[0].equals("img")){
+			list.appendTag(new NBTTagString(channel + "_img"));
+			list.appendTag(new NBTTagString(message));
+			list.appendTag(new NBTTagString(args[1].toString()));
+			list.appendTag(new NBTTagString(args[2].toString()));
+			list.appendTag(new NBTTagString(args[3].toString()));
+		}
+		else{
+			list.appendTag(new NBTTagString(channel));
+			list.appendTag(new NBTTagString(sender));
+			list.appendTag(new NBTTagString(message));
+			if(args != null) list.appendTag(new NBTTagString(args[0].toString()));
+		}
 		com.setTag("msg", list);
 		PacketHandler.getInstance().sendToAll(new PacketNBTTagCompound(com));
 	}
