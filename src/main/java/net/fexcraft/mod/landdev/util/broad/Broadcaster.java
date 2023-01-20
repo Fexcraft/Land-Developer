c092701817eb52a91dd1de2c6758b7cc86b8dee2
Ferdinand Calo <fercalo96@yahoo.de>
2023-01-20 21:09:14 +0100

    Update Broadcaster.java

--- src/main/java/net/fexcraft/mod/landdev/util/broad/Broadcaster.java
+++ src/main/java/net/fexcraft/mod/landdev/util/broad/Broadcaster.java
@@ -2,7 +2,7 @@ package net.fexcraft.mod.landdev.util.broad;
 
 import static net.fexcraft.mod.landdev.util.broad.BroadcastChannel.CHAT;
 
-import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ConcurrentHashMap;
 
 import javax.annotation.Nullable;
 
@@ -11,31 +11,56 @@ import net.fexcraft.mod.landdev.util.Settings;
 
 public class Broadcaster {
 	
-	public static ConcurrentLinkedQueue<Transmitter> SENDERS = new ConcurrentLinkedQueue<>();
-	static { SENDERS.add(new InternalTransmitter()); }
+	public static ConcurrentHashMap<TransmitterType, Transmitter> SENDERS = new ConcurrentHashMap<>();
+	static { SENDERS.put(TransmitterType.INTERNAL, new InternalTransmitter()); }
 
 	public static void send(Player player, String message){
-		send(CHAT.name, player.name(), message, player.adm ? Settings.CHAT_ADMIN_COLOR : Settings.CHAT_PLAYER_COLOR, false);
+		send(TargetTransmitter.ALL, CHAT.name, player.name(), message, player.adm ? Settings.CHAT_ADMIN_COLOR : Settings.CHAT_PLAYER_COLOR);
 	}
 
-	public static void send(BroadcastChannel channel, String sender, String message, String arg, boolean no_internal){
-		send(channel.name, sender, message, arg, no_internal);
+	public static void send(TargetTransmitter target, BroadcastChannel channel, String sender, String message, String... args){
+		send(target, channel.name, sender, message, args);
 	}
 
-	public static void send(String channel, String sender, String message, String arg, boolean no_internal){
-		for(Transmitter trs : SENDERS){
-			if(no_internal && trs.internal()) continue;
-			trs.transmit(channel, sender, message, arg);
+	public static void send(TargetTransmitter target, String channel, String sender, String message, String... args){
+		Transmitter trs = null;
+		for(TransmitterType type : target.types){
+			if((trs = SENDERS.get(type)) == null) continue;
+			trs.transmit(channel, sender, message, args);
 		}
 	}
 
 	public static interface Transmitter {
 		
-		public void transmit(String channel, String sender, String msg, @Nullable String color);
-		
-		public default boolean internal(){ return false; }
+		public void transmit(String channel, String sender, String msg, @Nullable String[] args);
 
-		public String category();
+		public default boolean internal(){ return false; }
+		
+		public TransmitterType type();
+		
+	}
+	
+	public static enum TransmitterType {
+		
+		INTERNAL, DISCORD;
+		
+		public boolean is(TransmitterType other){
+			return this == other;
+		}
+		
+	}
+	
+	public static enum TargetTransmitter {
+
+		ALL(TransmitterType.values()),
+		NO_INTERNAL(TransmitterType.DISCORD),
+		NO_DISCORD(TransmitterType.INTERNAL);
+
+		private TransmitterType[] types;
+
+		TargetTransmitter(TransmitterType... types){
+			this.types = types;
+		}
 		
 	}
 
