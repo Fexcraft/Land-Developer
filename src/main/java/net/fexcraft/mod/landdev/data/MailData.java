58725f82ad78125f577a23dfd82441ff40537a8c
Ferdinand Calo <fercalo96@yahoo.de>
2023-08-25 15:35:45 +0200

    Update MailData.java

--- src/main/java/net/fexcraft/mod/landdev/data/MailData.java
+++ src/main/java/net/fexcraft/mod/landdev/data/MailData.java
@@ -1,50 +1,78 @@
 package net.fexcraft.mod.landdev.data;
 
-import net.fexcraft.app.json.JsonMap;
-import net.minecraft.util.math.BlockPos;
+import java.util.ArrayList;
 
+import net.fexcraft.app.json.JsonArray;
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.app.json.JsonValue;
+import net.fexcraft.mod.landdev.util.ResManager;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
 public class MailData implements Saveable {
 
-	public static final String NOMAILBOX = "landdev.general.no_mailbox";
-	private BlockPos mailbox;
-	private int unread;
+	public ArrayList<Mail> mails = new ArrayList<>();
+	private String id;
+
+	public MailData(Layers lay, Object name){
+		id = lay.name() + "/" + name;
+	}
 
 	@Override
 	public void load(JsonMap map){
-		mailbox = map.has("mailbox") ? BlockPos.fromLong(map.get("mailbox").long_value()) : null;
+		if(map.has("mailbox")){
+			//marker detected, calling separate load instead
+			ResManager.load(this);
+			return;
+		}
+		if(map.has("mails")){
+			for(JsonValue<?> val : map.getArray("mails").value){
+				Mail mail = new Mail();
+				mail.load(val.asMap());
+				mails.add(mail);
+			}
+		}
 	}
 
 	@Override
 	public void save(JsonMap map){
-		if(mailbox != null) map.add("mailbox", mailbox.toLong());
-	}
-	
-	public BlockPos get(){
-		return mailbox;
-	}
-	
-	public void set(BlockPos pos){
-		mailbox = pos;
-	}
-
-	public boolean exists(){
-		return mailbox != null;
-	}
-
-	public boolean missing(){
-		return mailbox == null;
-	}
-
-	public void reset(){
-		set(null);
-	}
-
-	public String asString(){
-		return mailbox == null ? NOMAILBOX : mailbox.getX() + ", " + mailbox.getY() + ", " + mailbox.getZ();
+		if(!map.empty()){
+			//leaving a marker for load call and saving in separate table instead
+			map.add("mailbox", id);
+			save();
+		}
+		else{
+			map.add("id", id);
+			JsonArray array = new JsonArray();
+			for(Mail mail : mails){
+				JsonMap json = new JsonMap();
+				mail.save(json);
+				array.add(json);
+			}
+			map.add("mails", array);
+		}
 	}
 
 	public int unread(){
+		int unread = 0;
+		for(Mail mail : mails) if(mail.unread) unread++;
 		return unread;
+	}
+
+	@Override
+	public String saveId(){
+		return id;
+	}
+
+	@Override
+	public String saveTable(){
+		return "mails";
+	}
+
+	@Override
+	public void gendef(){
+		//
 	}
 
 }
