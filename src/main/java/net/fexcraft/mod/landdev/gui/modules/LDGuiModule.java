74e3cb200b1741df94e4337ae8b6188731bf4ade
Ferdinand Calo <fercalo96@yahoo.de>
2023-08-25 10:24:49 +0200

    Update LDGuiModule.java

--- src/main/java/net/fexcraft/mod/landdev/gui/modules/LDGuiModule.java
+++ src/main/java/net/fexcraft/mod/landdev/gui/modules/LDGuiModule.java
@@ -1,8 +1,6 @@
 package net.fexcraft.mod.landdev.gui.modules;
 
-import net.fexcraft.mod.landdev.data.player.Player;
 import net.fexcraft.mod.landdev.gui.LDGuiContainer;
-import net.minecraft.nbt.NBTTagCompound;
 
 public interface LDGuiModule {
 	
@@ -11,7 +9,7 @@ public interface LDGuiModule {
 
 	public void sync_packet(LDGuiContainer container, ModuleResponse resp);
 
-	public void on_interact(LDGuiContainer container, Player player, NBTTagCompound packet, String index);
+	public void on_interact(LDGuiContainer container, ModuleRequest req);
 	
 	public default boolean validateName(LDGuiContainer container, String name){
 		if(name.length() < 1){
