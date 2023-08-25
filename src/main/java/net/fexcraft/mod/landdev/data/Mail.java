bbcea68d2cd46c0957368debcdfbb5e92229a0d6
Ferdinand Calo <fercalo96@yahoo.de>
2023-08-25 17:32:11 +0200

    Update Mail.java

--- src/main/java/net/fexcraft/mod/landdev/data/Mail.java
+++ src/main/java/net/fexcraft/mod/landdev/data/Mail.java
@@ -16,9 +16,11 @@ public class Mail implements Saveable {
 	public Layers from;
 	public String fromid;
 	public String receiver;
+	public String title;
 	public ArrayList<String> message = new ArrayList<>();
 	public MailType type;
 	public long expiry;
+	public boolean staff;
 
 	@Override
 	public void save(JsonMap map){
@@ -26,9 +28,11 @@ public class Mail implements Saveable {
 		map.add("from", from.name());
 		map.add("from_id", fromid);
 		map.add("receiver", receiver);
+		map.add("title", title);
 		map.add("message", new JsonArray(message.toArray()));
 		map.add("type", type.name());
-		map.add("expiry", expiry);
+		if(expiry > 0) map.add("expiry", expiry);
+		if(staff) map.add("staffinv", staff);
 	}
 
 	@Override
@@ -37,9 +41,11 @@ public class Mail implements Saveable {
 		from = Layers.get(map.getString("from", Layers.NONE.name()));
 		fromid = map.getString("from_id", "System");
 		receiver = map.get("receiver", "error/unknown");
+		title = map.getString("title", "No Title");
 		message = map.getArray("message").toStringList();
 		type = MailType.valueOf(map.getString("type", "EXPIRED"));
 		expiry = map.getLong("expiry", 0);
+		staff = map.getBoolean("staffinv", false);
 	}
 
 	public int fromInt(){
@@ -51,7 +57,7 @@ public class Mail implements Saveable {
 	}
 
 	public boolean expired(){
-		return Time.getDate() > 0;
+		return expiry > 0 && Time.getDate() > expiry;
 	}
 
 }
