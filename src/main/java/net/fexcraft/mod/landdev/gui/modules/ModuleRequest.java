13211bb4eb421447b6a309fcb029ed9d6d1f4d7d
Ferdinand Calo <fercalo96@yahoo.de>
2023-08-25 10:24:58 +0200

    Create ModuleRequest.java

---
+++ src/main/java/net/fexcraft/mod/landdev/gui/modules/ModuleRequest.java
@@ -0,0 +1,60 @@
+package net.fexcraft.mod.landdev.gui.modules;
+
+import net.fexcraft.mod.landdev.data.player.Player;
+import net.fexcraft.mod.landdev.util.ResManager;
+import net.minecraft.nbt.NBTTagCompound;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class ModuleRequest {
+
+	private NBTTagCompound compound;
+	private String event;
+
+	public ModuleRequest(NBTTagCompound packet){
+		compound = new NBTTagCompound();
+		event = packet.getString("interact");
+	}
+
+	public NBTTagCompound getCompound(){
+		return compound;
+	}
+
+	public String event(){
+		return event;
+	}
+
+	public String getField(String key){
+		return compound.getCompoundTag("fields").getString(key);
+	}
+
+	public Player getPlayerField(String key){
+		return ResManager.getPlayer(getField(key), true);
+	}
+
+	public Player getPlayerField(String key, boolean load){
+		return ResManager.getPlayer(getField(key), load);
+	}
+
+	public String getRadio(){
+		return compound.getString("key");
+	}
+
+	public String getRadio(String rep){
+		return compound.getString("key").replace("rep", "");
+	}
+
+	public int getRadioInt(String rep){
+		return Integer.parseInt(getRadio(rep));
+	}
+
+	public float getRadioFloat(String rep){
+		return Float.parseFloat(getRadio(rep));
+	}
+
+	public boolean getCheck(String key){
+		return compound.getCompoundTag("checkboxes").getBoolean(key);
+	}
+
+}
