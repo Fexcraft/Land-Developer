ef5f19bb85b9fd0fca0838b072b6e8dd2f5080e6
Ferdinand Calo <fercalo96@yahoo.de>
2023-08-25 10:24:53 +0200

    Update Missing.java

--- src/main/java/net/fexcraft/mod/landdev/gui/modules/Missing.java
+++ src/main/java/net/fexcraft/mod/landdev/gui/modules/Missing.java
@@ -1,8 +1,6 @@
 package net.fexcraft.mod.landdev.gui.modules;
 
-import net.fexcraft.mod.landdev.data.player.Player;
 import net.fexcraft.mod.landdev.gui.LDGuiContainer;
-import net.minecraft.nbt.NBTTagCompound;
 
 public class Missing implements LDGuiModule {
 	
@@ -13,7 +11,7 @@ public class Missing implements LDGuiModule {
 		resp.setTitle("missing.title");
 	}
 
-	public void on_interact(LDGuiContainer container, Player player, NBTTagCompound packet, String index){
+	public void on_interact(LDGuiContainer container, ModuleRequest req){
 		//
 	}
 
