edc4887ba9f3a9e3de1d6c4a123d8ed5d5ec34df
Ferdinand Calo <fercalo96@yahoo.de>
2023-08-27 15:14:03 +0200

    Update LandDev.java

--- src/main/java/net/fexcraft/mod/landdev/LandDev.java
+++ src/main/java/net/fexcraft/mod/landdev/LandDev.java
@@ -49,7 +49,7 @@ public class LandDev {
 	
     public static final String MODID = "landdev";
     public static final String NAME = "LandDev";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
 	@Mod.Instance(MODID)
 	public static LandDev INSTANCE;
 	public static Database DB = new JsonFileDB();
