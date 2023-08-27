28bf536ab917c0b05a280e71b1147f5d5464b00e
Ferdinand Calo <fercalo96@yahoo.de>
2023-08-27 12:30:30 +0200

    Update Norms.java

--- src/main/java/net/fexcraft/mod/landdev/data/Norms.java
+++ src/main/java/net/fexcraft/mod/landdev/data/Norms.java
@@ -1,13 +1,13 @@
 package net.fexcraft.mod.landdev.data;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 import net.fexcraft.app.json.JsonMap;
 import net.fexcraft.mod.landdev.data.norm.Norm;
 
 public class Norms implements Saveable {
 	
-	public HashMap<String, Norm> norms = new HashMap<>();
+	public LinkedHashMap<String, Norm> norms = new LinkedHashMap<>();
 	
 	public void add(Norm norm){
 		norms.put(norm.id, norm);
@@ -34,6 +34,24 @@ public class Norms implements Saveable {
 
 	public Norm get(String key){
 		return norms.get(key);
+	}
+
+	public int index(Norm norm){
+		int idx = 0;
+		for(Norm nor : norms.values()){
+			if(nor == norm) return idx;
+			idx++;
+		}
+		return 0;
+	}
+
+	public Norm get(int index){
+		int idx = 0;
+		for(Norm norm : norms.values()){
+			if(idx == index) return norm;
+			idx++;
+		}
+		return null;
 	}
 
 }
