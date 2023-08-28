28768bab54870ff51884862b95047698a4561d82
Ferdinand Calo <fercalo96@yahoo.de>
2023-08-28 12:46:10 +0200

    Update Municipality.java

--- src/main/java/net/fexcraft/mod/landdev/data/municipality/Municipality.java
+++ src/main/java/net/fexcraft/mod/landdev/data/municipality/Municipality.java
@@ -1,15 +1,17 @@
 package net.fexcraft.mod.landdev.data.municipality;
 
-import static net.fexcraft.mod.landdev.data.PermAction.*;
-import static net.fexcraft.mod.landdev.gui.LDGuiElementType.ELM_BLUE;
-import static net.fexcraft.mod.landdev.gui.LDGuiElementType.ELM_GENERIC;
-import static net.fexcraft.mod.landdev.gui.LDGuiElementType.ICON_BLANK;
-import static net.fexcraft.mod.landdev.gui.LDGuiElementType.ICON_OPEN;
+import static net.fexcraft.mod.landdev.data.PermAction.CREATE_MUNICIPALITY;
+import static net.fexcraft.mod.landdev.data.PermAction.MANAGE_MUNICIPALITY;
+import static net.fexcraft.mod.landdev.data.PermAction.MUNICIPALITY_CITIZEN;
+import static net.fexcraft.mod.landdev.data.PermAction.MUNICIPALITY_STAFF;
+import static net.fexcraft.mod.landdev.gui.GuiHandler.MAILBOX;
+import static net.fexcraft.mod.landdev.gui.LDGuiElementType.*;
 import static net.fexcraft.mod.landdev.util.ResManager.SERVER_ACCOUNT;
 import static net.fexcraft.mod.landdev.util.TranslationUtil.translate;
 import static net.fexcraft.mod.landdev.util.TranslationUtil.translateCmd;
 
 import java.util.ArrayList;
+import java.util.Map.Entry;
 
 import net.fexcraft.app.json.JsonArray;
 import net.fexcraft.app.json.JsonMap;
@@ -22,11 +24,12 @@ import net.fexcraft.mod.landdev.data.*;
 import net.fexcraft.mod.landdev.data.chunk.Chunk_;
 import net.fexcraft.mod.landdev.data.county.County;
 import net.fexcraft.mod.landdev.data.district.District;
-import net.fexcraft.mod.landdev.data.norm.BoolNorm;
+import net.fexcraft.mod.landdev.data.norm.Norm;
 import net.fexcraft.mod.landdev.data.norm.StringNorm;
 import net.fexcraft.mod.landdev.data.player.Permit;
 import net.fexcraft.mod.landdev.data.player.Player;
 import net.fexcraft.mod.landdev.gui.LDGuiContainer;
+import net.fexcraft.mod.landdev.gui.LDGuiElementType;
 import net.fexcraft.mod.landdev.gui.modules.LDGuiModule;
 import net.fexcraft.mod.landdev.gui.modules.ModuleRequest;
 import net.fexcraft.mod.landdev.gui.modules.ModuleResponse;
@@ -55,13 +58,8 @@ public class Municipality implements Saveable, Layer, LDGuiModule {
 		this.id = id;
 		mail = new MailData(getLayer(), id);
 		account = DataManager.getAccount("municipality:" + id, false, true);
-		norms.add(new StringNorm("name", translate("municipality.norm.name")));
-		manage.norms.add(new BoolNorm("claim", false));
-		manage.norms.add(new BoolNorm("manage_district", false));
-		manage.norms.add(new BoolNorm("set_tax_chunk", false));
-		manage.norms.add(new BoolNorm("set_tax_player", false));
-		manage.norms.add(new BoolNorm("finances_use", false));
-		manage.norms.add(new BoolNorm("finances_manage", false));
+		norms.add(new StringNorm("name", ""));
+		norms.add(new StringNorm("title", ""));
 	}
 
 	@Override
@@ -106,18 +104,23 @@ public class Municipality implements Saveable, Layer, LDGuiModule {
 	public void gendef(){
 		if(id == -1){
 			norms.get("name").set(translate("municipality.wilderness.name"));
+			norms.get("title").set(translate("municipality.wilderness.title"));
 			districts.clear();
 			county = ResManager.getCounty(-1, true);
 			color.set(0x009900);
 		}
 		else if(id == 0){
 			norms.get("name").set(translate("municipality.spawnzone.name"));
+			norms.get("title").set(translate("municipality.spawnzone.title"));
 			districts.clear();
 			districts.add(0);
 			county = ResManager.getCounty(0, true);
 			color.set(0xff9900);
 		}
-		else return;
+		else{
+			norms.get("name").set(translate("municipality.unnamed.name"));
+			norms.get("title").set(translate("municipality.unnamed.title"));
+		};
 	}
 	
 	@Override
@@ -143,39 +146,198 @@ public class Municipality implements Saveable, Layer, LDGuiModule {
 	public String name(){
 		return norms.get("name").string();
 	}
+
+	public String title(){
+		return norms.get("title").string();
+	}
 	
 	public static final int UI_CREATE = -1;
+	public static final int UI_STAFF = 1;
+	public static final int UI_PRICE = 2;
+	public static final int UI_SET_PRICE = 3;
+	public static final int UI_NORMS = 4;
+	public static final int UI_NORM_EDIT = 5;
+	public static final int UI_APPREARANCE = 6;
+
 
 	@Override
 	public void sync_packet(LDGuiContainer container, ModuleResponse resp){
 		resp.setTitle("municipality.title");
 		NBTTagList list = new NBTTagList();
+		boolean canman = manage.can(MANAGE_MUNICIPALITY, container.player.uuid) || container.player.adm;
 		switch(container.x){
-		case UI_CREATE:
-			resp.setTitle("municipality.create.title");
-			Chunk_ chunk = ResManager.getChunk(container.player.entity);
-    		County county = chunk.district.county();
-    		boolean cn = county.norms.get("new-municipalities").bool();
-    		boolean pp = container.player.hasPermit(CREATE_MUNICIPALITY, county.getLayer(), county.id);
-    		if(!cn && !pp){
-    			resp.addRow("create.no_perm", ELM_GENERIC, ICON_BLANK);
-    			break;
-    		}
-			resp.addRow("create.name", ELM_GENERIC);
-			resp.addField("create.name_field");
-			resp.addCheck("create.county_funded", ELM_GENERIC, pp);
-			resp.addCheck("create.claim_district", ELM_GENERIC, pp);
-			resp.addButton("create.submit", ELM_BLUE, ICON_OPEN);
-			resp.setFormular();
-			resp.setNoBack();
-			break;
+			case UI_MAIN:{
+				resp.addRow("id", ELM_GENERIC, id);
+				resp.addRow("name", ELM_GENERIC, canman ? ICON_OPEN : ICON_EMPTY, canman, name());
+				resp.addRow("muntitle", ELM_GENERIC, canman ? ICON_OPEN : ICON_EMPTY, canman, title());
+				resp.addButton("county", ELM_GENERIC, ICON_OPEN, county.name());
+				resp.addRow("manager", ELM_GENERIC, manage.getManagerName());
+				resp.addButton("staff", ELM_GENERIC, ICON_OPEN, manage.staff.size());
+				resp.addBlank();
+				if(sell.price > 0){
+					resp.addButton("price", ELM_GENERIC, ICON_OPEN, sell.price_formatted());
+				}
+				if(canman){
+					resp.addButton("set_price", ELM_GENERIC, ICON_OPEN);
+				}
+				if(sell.price > 0) resp.addBlank();
+				if(canman){
+					resp.addButton("mailbox", ELM_GENERIC, ICON_OPEN, mail.unread());
+				}
+				resp.addButton("norms", ELM_GREEN, ICON_OPEN);
+				resp.addButton("appearance", ELM_YELLOW, ICON_OPEN);
+				break;
+			}
+			case UI_PRICE:
+				resp.setTitle("municipality.buy.title");
+				resp.addRow("id", ELM_GENERIC, ICON_BLANK, id);
+				resp.addRow("buy.info0", ELM_YELLOW, ICON_BLANK, null);
+				resp.addRow("buy.info1", ELM_YELLOW, ICON_BLANK, null);
+				resp.addButton("buy.county_pays", ELM_GENERIC, ICON_CHECKBOX_UNCHECKED);
+				resp.addButton("buy.submit", ELM_GENERIC, ICON_OPEN);
+				resp.setFormular();
+				break;
+			case UI_SET_PRICE:
+				resp.setTitle("municipality.set_price.title");
+				resp.addRow("id", ELM_GENERIC, ICON_BLANK, id);
+				resp.addField("set_price.field");
+				resp.addButton("set_price.submit", ELM_GENERIC, ICON_OPEN);
+				resp.setFormular();
+				break;
+			case UI_APPREARANCE:
+				resp.setTitle("municipality.appearance.title");
+				resp.addRow("appearance.icon", ELM_GENERIC);
+				resp.addField("appearance.icon_field", icon.getn());
+				resp.addRow("appearance.color", ELM_GENERIC);
+				resp.addField("appearance.color_field", color.getString());
+				if(canman){
+					resp.addRow("appearance.submit", ELM_GENERIC, ICON_OPEN);
+					resp.setFormular();
+				}
+				break;
+			case UI_NORMS:
+				resp.setTitle("municipality.norms");
+				LDGuiElementType icon = canman ? ICON_OPEN : ICON_EMPTY;
+				for(Entry<String, Norm> entry : norms.norms.entrySet()){
+					if(entry.getValue().type.isBool()){
+						resp.addRow("norm." + entry.getKey(), ELM_GENERIC, icon, canman, entry.getValue().bool() ? LANG_YES : LANG_NO);
+					}
+					else{
+						resp.addRow("norm." + entry.getKey(), ELM_GENERIC, icon, canman, entry.getValue().string());
+					}
+				}
+				break;
+			case UI_NORM_EDIT:{
+				resp.setTitle("municipality.norm_editor");
+				Norm norm = norms.get(container.z);
+				if(norm == null) return;
+				resp.addRow("norm_id", ELM_GREEN, norm.id);
+				resp.addRow("norm_value", ELM_GENERIC, norm.string());
+				if(!canman) break;
+				if(norm.type.isBool()){
+					resp.addButton("norm_bool", ELM_BLUE, norm.bool() ? ICON_ENABLED : ICON_DISABLED);
+				}
+				else{
+					resp.addField("norm_field", norm.string());
+					resp.addButton("norm_submit", ELM_BLUE, ICON_OPEN);
+					resp.setFormular();
+				}
+				break;
+			}
+			case UI_CREATE:
+				resp.setTitle("municipality.create.title");
+				Chunk_ chunk = ResManager.getChunk(container.player.entity);
+				County county = chunk.district.county();
+				boolean cn = county.norms.get("new-municipalities").bool();
+				boolean pp = container.player.hasPermit(CREATE_MUNICIPALITY, county.getLayer(), county.id);
+				if(!cn && !pp){
+					resp.addRow("create.no_perm", ELM_GENERIC, ICON_BLANK);
+					break;
+				}
+				resp.addRow("create.name", ELM_GENERIC);
+				resp.addField("create.name_field");
+				resp.addCheck("create.county_funded", ELM_GENERIC, pp);
+				resp.addCheck("create.claim_district", ELM_GENERIC, pp);
+				resp.addButton("create.submit", ELM_BLUE, ICON_OPEN);
+				resp.setFormular();
+				resp.setNoBack();
+				break;
 		}
 	}
 
 	@Override
 	public void on_interact(LDGuiContainer container, ModuleRequest req){
 		Player player = container.player;
+		boolean canman = manage.can(MANAGE_MUNICIPALITY, container.player.uuid) || container.player.adm;
 		switch(req.event()){
+			case "name":{
+				if(!canman) return;
+				container.open(UI_NORM_EDIT, id, norms.index(norms.get("name")));
+				return;
+			}
+			case "muntitle":{
+				if(!canman) return;
+				container.open(UI_NORM_EDIT, id, norms.index(norms.get("title")));
+				return;
+			}
+			case "price": container.open(UI_PRICE); return;
+			case "set_price": if(canman) container.open(UI_SET_PRICE); return;
+			case "mailbox": if(canman) container.open(MAILBOX, getLayer().ordinal(), id, 0); return;
+			case "norms": container.open(UI_NORMS); return;
+			case "appearance": container.open(UI_APPREARANCE); return;
+			case "buy.submit":{
+
+				return;
+			}
+			case "set_price.submit":{
+				if(!canman) return;
+				String[] err = new String[]{ "" };
+				String val = req.getField("set_price.field");
+				long value = Settings.format_price(err, val);
+				if(err[0].length() > 0){
+					container.sendMsg(err[0], false);
+				}
+				else{
+					sell.price = value;
+					container.open(UI_MAIN);
+				}
+				return;
+			}
+			case "norm_submit":{
+				if(!canman) return;
+				Norm norm = norms.get(container.z);
+				if(norm == null) return;
+				if(norm.type.isInteger()){
+					try{
+						norm.set(Integer.parseInt(req.getField("norm_field")));
+					}
+					catch(Exception e){
+						container.sendMsg("Error: " + e.getMessage());
+					}
+				}
+				else if(norm.type.isDecimal()){
+					try{
+						norm.set(Float.parseFloat(req.getField("norm_field")));
+					}
+					catch(Exception e){
+						container.sendMsg("Error: " + e.getMessage());
+					}
+				}
+				else{
+					norm.set(req.getField("norm_field"));
+				}
+				container.open(UI_NORM_EDIT);
+				return;
+			}
+			case "norm_bool":{
+				if(!canman) return;
+				Norm norm = norms.get(container.z);
+				if(norm == null) return;
+				if(!norm.type.isBool()) return;
+				norm.toggle();
+				container.open(UI_NORM_EDIT);
+				return;
+			}
 			case "create.submit":{
 				Chunk_ chunk = ResManager.getChunk(container.player.entity);
 				County county = chunk.district.county();
@@ -275,6 +437,12 @@ public class Municipality implements Saveable, Layer, LDGuiModule {
     			Announcer.announce(Announcer.Target.GLOBAL, 0, "announce.new_municipality", name, newid);
 				return;
 			}
+		}
+		if(req.event().startsWith("norm.")){
+			Norm norm = norms.get(req.event().substring(5));
+			if(norm == null) return;
+			container.open(UI_NORM_EDIT, id, norms.index(norm));
+			return;
 		}
 	}
 
