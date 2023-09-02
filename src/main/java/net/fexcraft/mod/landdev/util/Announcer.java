a6f7a205fab7a543a49c0364dfc265f07a647183
Ferdinand Calo <fercalo96@yahoo.de>
2023-09-02 13:54:20 +0200

    Update Announcer.java

--- src/main/java/net/fexcraft/mod/landdev/util/Announcer.java
+++ src/main/java/net/fexcraft/mod/landdev/util/Announcer.java
@@ -2,26 +2,43 @@ package net.fexcraft.mod.landdev.util;
 
 import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.lib.mc.utils.Static;
+import net.fexcraft.mod.landdev.data.player.Player;
 import net.minecraft.entity.player.EntityPlayerMP;
 
 public class Announcer {
 
 	public static void announce(Target target, int id, String string, Object... objs){
 		switch(target){
-			case COMPANY:
-				break;
-			case COUNTY:
-				break;
-			case DISTRICT:
-				break;
 			case GLOBAL:
 				Static.getServer().getPlayerList().getPlayers().forEach(player -> { announce(player, string, objs); });
 				break;
 			case LOCAL:
 				break;
+			case COMPANY:
+				break;
+			case DISTRICT:
+				Static.getServer().getPlayerList().getPlayers().forEach(player -> {
+					Player ply = ResManager.getPlayer(player);
+					if(ply.isCurrentlyInDistrict(id)) announce(player, string, objs);
+				});
+				break;
 			case MUNICIPALITY:
+				Static.getServer().getPlayerList().getPlayers().forEach(player -> {
+					Player ply = ResManager.getPlayer(player);
+					if(ply.municipality.id == id || ply.isCurrentlyInMunicipality(id)) announce(player, string, objs);
+				});
+				break;
+			case COUNTY:
+				Static.getServer().getPlayerList().getPlayers().forEach(player -> {
+					Player ply = ResManager.getPlayer(player);
+					if(ply.county.id == id || ply.isCurrentlyInCounty(id)) announce(player, string, objs);
+				});
 				break;
 			case STATE:
+				Static.getServer().getPlayerList().getPlayers().forEach(player -> {
+					Player ply = ResManager.getPlayer(player);
+					if(ply.county.state.id == id || ply.isCurrentlyInState(id)) announce(player, string, objs);
+				});
 				break;
 			default: return;
 		}
