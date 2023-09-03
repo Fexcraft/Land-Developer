e87300eab41da9918201e2fa48cbb3a802f0f4fa
Ferdinand Calo <fercalo96@yahoo.de>
2023-09-03 20:01:57 +0200

    Update MailModule.java

--- src/main/java/net/fexcraft/mod/landdev/gui/modules/MailModule.java
+++ src/main/java/net/fexcraft/mod/landdev/gui/modules/MailModule.java
@@ -2,18 +2,23 @@ package net.fexcraft.mod.landdev.gui.modules;
 
 import static net.fexcraft.mod.landdev.data.PermAction.MAIL_DELETE;
 import static net.fexcraft.mod.landdev.data.PermAction.MAIL_READ;
+import static net.fexcraft.mod.landdev.data.PermAction.MUNICIPALITY_STAFF;
 import static net.fexcraft.mod.landdev.gui.LDGuiElementType.*;
+import static net.fexcraft.mod.landdev.util.TranslationUtil.translate;
 
 import net.fexcraft.lib.common.math.Time;
+import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.mod.landdev.data.Layers;
 import net.fexcraft.mod.landdev.data.Mail;
 import net.fexcraft.mod.landdev.data.MailData;
 import net.fexcraft.mod.landdev.data.MailType;
+import net.fexcraft.mod.landdev.data.Manageable.Staff;
 import net.fexcraft.mod.landdev.data.county.County;
 import net.fexcraft.mod.landdev.data.district.District;
 import net.fexcraft.mod.landdev.data.municipality.Municipality;
 import net.fexcraft.mod.landdev.data.player.Player;
 import net.fexcraft.mod.landdev.data.state.State;
+import net.fexcraft.mod.landdev.gui.GuiHandler;
 import net.fexcraft.mod.landdev.gui.LDGuiContainer;
 import net.fexcraft.mod.landdev.util.ResManager;
 import net.fexcraft.mod.landdev.util.Settings;
@@ -70,24 +75,83 @@ public class MailModule implements LDGuiModule {
 	}
 
 	public void on_interact(LDGuiContainer container, ModuleRequest req){
+		MailData mailbox = getMailbox(container.player, container.x, container.y);
+		if(mailbox == null) return;
+		Player player = container.player;
+		Mail mail = container.z >= mailbox.mails.size() ? null : mailbox.mails.get(container.z);
 		switch(req.event()){
 			case "invite.accept":{
+				if(mail.type != MailType.INVITE) return;
+				switch(mail.from){
+					case MUNICIPALITY:{
+						if(mail.receiver.is(Layers.PLAYER)){
+							player = ResManager.getPlayer(mail.recUUID(), true);
+							Municipality mun = ResManager.getMunicipality(mail.fromInt(), true);
+							if(mail.staff){
+								if(!mun.citizens.isCitizen(player.uuid)){
+									Print.chat(player.entity, translate("mail.municipality.staff.notmember"));
+									return;
+								}
+								mun.manage.staff.add(new Staff(player.uuid, MUNICIPALITY_STAFF));
+								mun.save();
+								String pln = ResManager.getPlayerName(player.uuid);
+								for(Staff stf : mun.manage.staff){
+									Player stp = ResManager.getPlayer(stf.uuid, true);
+									mail = new Mail(MailType.SYSTEM, Layers.MUNICIPALITY, mun.id, Layers.PLAYER, stp.uuid).expireInDays(7);
+									mail.setTitle(mun.name()).addMessage(translate("mail.municipality.staff.added", pln));
+									stp.addMailAndSave(mail);
+								}
+							}
+							else{
+								if(player.isMunicipalityManager()){
+									Print.chat(player.entity, translate("mail.municipality.citizen.ismanager"));
+									return;
+								}
+								if(player.isCountyManager() && mun.county.id != player.county.id){
+									Print.chat(player.entity, translate("mail.county.citizen.ismanager"));
+									return;
+								}
+								player.setCitizenOf(mun);
+								//TODO announce
+							}
+						}
+						else if(mail.receiver.is(Layers.MUNICIPALITY)){
+							//invites into a county
+						}
+						return;
+					}
+					case COUNTY:{
 
+						return;
+					}
+					case STATE:{
+
+						return;
+					}
+					case COMPANY:{
+						//
+						return;
+					}
+				}
 				return;
 			}
 			case "invite.reject":{
-
+				if(mail.type != MailType.INVITE) return;
+				mail.expiry(Time.getDate());
+				container.open(GuiHandler.MAILBOX, container.x, container.y, container.z);
 				return;
 			}
 			case "request.accept":{
-
+				if(mail.type != MailType.REQUEST) return;
 				return;
 			}
 			case "request.reject":{
+				if(mail.type != MailType.REQUEST) return;
 
 				return;
 			}
 			case "request.timeout":{
+				if(mail.type != MailType.REQUEST) return;
 
 				return;
 			}
