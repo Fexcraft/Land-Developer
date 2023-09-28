c35ae440b55cab807414fd37c1b7c08172c9bdeb
Ferdinand Calo <fercalo96@yahoo.de>
2023-09-28 17:16:23 +0200

    Update LDGuiClaimCon.java

--- src/main/java/net/fexcraft/mod/landdev/gui/LDGuiClaimCon.java
+++ src/main/java/net/fexcraft/mod/landdev/gui/LDGuiClaimCon.java
@@ -1,6 +1,7 @@
 package net.fexcraft.mod.landdev.gui;
 
 import static net.fexcraft.mod.landdev.data.PermAction.CHUNK_CLAIM;
+import static net.fexcraft.mod.landdev.util.ResManager.SERVER_ACCOUNT;
 
 import java.util.HashMap;
 
@@ -8,8 +9,12 @@ import net.fexcraft.lib.common.math.RGB;
 import net.fexcraft.lib.mc.gui.GenericContainer;
 import net.fexcraft.lib.mc.utils.Formatter;
 import net.fexcraft.lib.mc.utils.Print;
+import net.fexcraft.mod.fsmm.api.Bank;
+import net.fexcraft.mod.fsmm.api.Bank.Action;
+import net.fexcraft.mod.fsmm.util.DataManager;
 import net.fexcraft.mod.landdev.data.chunk.Chunk_;
 import net.fexcraft.mod.landdev.data.district.District;
+import net.fexcraft.mod.landdev.data.player.Player;
 import net.fexcraft.mod.landdev.util.ResManager;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
@@ -24,6 +29,7 @@ public class LDGuiClaimCon extends GenericContainer {
 	protected ChunkData[][] chunks = new ChunkData[15][15];
 	protected HashMap<Integer, DisData> dists = new HashMap<>();
 	protected District district;
+	protected Player ldp;
 	public final int x, z, dis;
 	@SideOnly(Side.CLIENT)
 	public LDGuiClaim gui;
@@ -42,56 +48,65 @@ public class LDGuiClaimCon extends GenericContainer {
 		else{
 			district = ResManager.getDistrict(dis, false);
 		}
+		ldp = ResManager.getPlayer(player);
 	}
 
 	@Override
 	protected void packet(Side side, NBTTagCompound packet, EntityPlayer player){
 		if(side.isServer()){
 			if(packet.hasKey("sync")){
-				NBTTagCompound compound = new NBTTagCompound();
-				compound.setBoolean("ckdata", true);
-				NBTTagList list = new NBTTagList();
-				HashMap<Integer, District> dis = new HashMap<>();
-				Chunk_ chunk = null;
-				for(int i = -7; i < 8; i++){
-					for(int k = -7; k < 8; k++){
-						chunk = ResManager.getChunk(x + i, z + k);
-						NBTTagCompound com = new NBTTagCompound();
-						com.setInteger("c", chunk.district.color.getInteger());
-						com.setInteger("d", chunk.district.id);
-						com.setLong("p", chunk.sell.price);
-						list.appendTag(com);
-						if(!dis.containsKey(chunk.district.id)) dis.put(chunk.district.id, chunk.district);
-					}
-				}
-				compound.setTag("cks", list);
-				list = new NBTTagList();
-				for(District d : dis.values()){
-					NBTTagCompound com = new NBTTagCompound();
-					com.setInteger("i", d.id);
-					com.setString("n", d.name());
-					com.setInteger("o", d.owner.owid);
-					com.setString("m", d.owner.name());
-					com.setBoolean("c", d.owner.is_county);
-					list.appendTag(com);
-				}
-				compound.setTag("dis", list);
-				send(Side.CLIENT, compound);
+				sendSync(new NBTTagCompound());
 			}
 			else if(packet.hasKey("claim")){
 				int[] key = packet.getIntArray("claim");
 				Chunk_ chunk = ResManager.getChunk(key[0] - 7 + x, key[1] - 7 + z);
 				NBTTagCompound com = new NBTTagCompound();
-				if(!district.can(CHUNK_CLAIM, player.getGameProfile().getId())){
+				if(chunk.sell.price == 0){
+					com.setString("msg", "landdev.gui.claim.not_for_sale");
+					send(Side.CLIENT, com);
+					return;
+				}
+				if(!ldp.adm && !district.can(CHUNK_CLAIM, player.getGameProfile().getId())){
 					com.setString("msg", "landdev.gui.claim.no_perm_district");
+					send(Side.CLIENT, com);
+					return;
 				}
-				else{
-					com.setString("msg", "pass");
-					if(chunk.district.id > -1) chunk.district.chunks -= 1;
-					chunk.district = district;
-					chunk.district.chunks += 1;
+				if(chunk.district.id >= 0){
+					if(chunk.district.owner.is_county){
+						if(district.owner.is_county){
+							com.setString("msg", "landdev.gui.claim.county_chunk");
+							send(Side.CLIENT, com);
+							return;
+						}
+						else if(chunk.district.county().id != district.county().id){
+							com.setString("msg", "landdev.gui.claim.county_other");
+							send(Side.CLIENT, com);
+							return;
+						}
+					}
+					else{
+						if(district.owner.is_county){
+							com.setString("msg", "landdev.gui.claim.municipality_chunk");
+							send(Side.CLIENT, com);
+							return;
+						}
+						else if(chunk.district.municipality().id != district.municipality().id){
+							com.setString("msg", "landdev.gui.claim.municipality_other");
+							send(Side.CLIENT, com);
+							return;
+						}
+					}
 				}
-				send(Side.CLIENT, com);
+				Bank bank = DataManager.getBank(district.owner.account().getBankId(), false, true);
+				if(!bank.processAction(Action.TRANSFER, player, district.account(), chunk.sell.price, SERVER_ACCOUNT)) return;
+				com.setString("msg", "landdev.gui.claim.pass");
+				if(chunk.district.id > -1) chunk.district.chunks -= 1;
+				chunk.district = district;
+				chunk.district.chunks += 1;
+				chunk.sell.price = 0;
+				if(chunk.district.id < 0) chunk.created.setClaimer(player.getGameProfile().getId());
+				chunk.save();
+				sendSync(com);
 			}
 		}
 		else{
@@ -116,12 +131,43 @@ public class LDGuiClaimCon extends GenericContainer {
 					dists.put(com.getInteger("i"), new DisData(com));
 				}
 			}
-			else if(packet.hasKey("msg")){
+			if(packet.hasKey("msg")){
 				LDGuiClaim.title.string = Formatter.format(I18n.format(packet.getString("msg")));
 			}
 		}
 	}
-	
+
+	private void sendSync(NBTTagCompound compound){
+		compound.setBoolean("ckdata", true);
+		NBTTagList list = new NBTTagList();
+		HashMap<Integer, District> dis = new HashMap<>();
+		Chunk_ chunk = null;
+		for(int i = -7; i < 8; i++){
+			for(int k = -7; k < 8; k++){
+				chunk = ResManager.getChunk(x + i, z + k);
+				NBTTagCompound com = new NBTTagCompound();
+				com.setInteger("c", chunk.district.color.getInteger());
+				com.setInteger("d", chunk.district.id);
+				com.setLong("p", chunk.sell.price);
+				list.appendTag(com);
+				if(!dis.containsKey(chunk.district.id)) dis.put(chunk.district.id, chunk.district);
+			}
+		}
+		compound.setTag("cks", list);
+		list = new NBTTagList();
+		for(District d : dis.values()){
+			NBTTagCompound com = new NBTTagCompound();
+			com.setInteger("i", d.id);
+			com.setString("n", d.name());
+			com.setInteger("o", d.owner.owid);
+			com.setString("m", d.owner.name());
+			com.setBoolean("c", d.owner.is_county);
+			list.appendTag(com);
+		}
+		compound.setTag("dis", list);
+		send(Side.CLIENT, compound);
+	}
+
 	public static class ChunkData {
 
 		protected RGB color = new RGB();
