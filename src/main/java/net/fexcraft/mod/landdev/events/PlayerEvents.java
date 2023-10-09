b6226bcb5b07a03f204391a92ae9fe2e1c5ce04f
Ferdinand Calo <fercalo96@yahoo.de>
2023-10-09 19:35:23 +0200

    Update PlayerEvents.java

--- src/main/java/net/fexcraft/mod/landdev/events/PlayerEvents.java
+++ src/main/java/net/fexcraft/mod/landdev/events/PlayerEvents.java
@@ -64,6 +64,7 @@ public class PlayerEvents {
 			player.last_pos_update = time;
 			player.chunk_last = player.chunk_current;
 			player.chunk_current = ResManager.getChunk(event.player);
+			//if(player.chunk_current == null) player.chunk_last = player.chunk_current;
 			if(player.chunk_last == null) player.chunk_last = player.chunk_current;
 			moved = player.chunk_current.district.id != player.chunk_last.district.id;
 			label = player.chunk_current.label.present && player.chunk_current != player.chunk_last;
