7c61c6f3c9da1cb731581d1f733c5c46eb6bf7f9
Ferdinand Calo <fexcalo@gmail.com>
2024-03-07 17:04:22 +0100

    Update Layer.java

--- src/main/java/net/fexcraft/mod/landdev/data/Layer.java
+++ src/main/java/net/fexcraft/mod/landdev/data/Layer.java
@@ -3,11 +3,15 @@ package net.fexcraft.mod.landdev.data;
 public interface Layer {
 
 	public Layers getLayer();
-	
+
 	public default boolean is(Layers lay){
 		return lay == getLayer();
 	}
-	
+
 	public Layers getParentLayer();
-	
+
+	public default int id(){
+		return 0;
+	}
+
 }
