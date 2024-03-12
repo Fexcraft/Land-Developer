bef27ae5f7d7f14021b912752534ba56b5d5ea86
Ferdinand Calo <fexcalo@gmail.com>
2024-03-12 22:40:48 +0100

    Update ChunkCapImpl.java

--- src/main/java/net/fexcraft/mod/landdev/data/chunk/cap/ChunkCapImpl.java
+++ src/main/java/net/fexcraft/mod/landdev/data/chunk/cap/ChunkCapImpl.java
@@ -1,14 +1,14 @@
 package net.fexcraft.mod.landdev.data.chunk.cap;
 
 import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.landdev.data.chunk.ChunkRegion;
 import net.fexcraft.mod.landdev.data.chunk.Chunk_;
 import net.fexcraft.mod.landdev.util.ResManager;
+import net.fexcraft.mod.landdev.util.Settings;
 import net.minecraft.world.chunk.Chunk;
 
 /**
- *
  * @author Ferdnand Calo' (FEX___96)
- *
  */
 public class ChunkCapImpl implements ChunkCap {
 
@@ -16,9 +16,14 @@ public class ChunkCapImpl implements ChunkCap {
 
 	@Override
 	public void setChunk(Chunk ck){
-		chunk = new Chunk_(ck);
-		chunk.load(new JsonMap());
-		ResManager.CHUNKS.put(chunk.key, chunk);
+		if(Settings.SAVE_CHUNKS_IN_REGIONS){
+			chunk = ChunkRegion.load(ck);
+		}
+		else{
+			chunk = new Chunk_(ck);
+			chunk.load(new JsonMap());
+			ResManager.CHUNKS.put(chunk.key, chunk);
+		}
 	}
 
 	@Override
