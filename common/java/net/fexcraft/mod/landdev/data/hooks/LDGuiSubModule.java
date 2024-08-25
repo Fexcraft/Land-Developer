1c69d719a6ecee6740d9aadbfaf819cc627e1cb7
Ferdinand Calo <fexcalo@gmail.com>
2024-08-25 18:20:42 +0200

    Update LDGuiSubModule.java

--- common/java/net/fexcraft/mod/landdev/data/hooks/LDGuiSubModule.java
+++ common/java/net/fexcraft/mod/landdev/data/hooks/LDGuiSubModule.java
@@ -1,17 +1,17 @@
 package net.fexcraft.mod.landdev.data.hooks;
 
 import net.fexcraft.mod.landdev.gui.LDGuiContainer;
-import net.fexcraft.mod.landdev.gui.modules.LDGuiModule;
-import net.fexcraft.mod.landdev.gui.modules.ModuleRequest;
-import net.fexcraft.mod.landdev.gui.modules.ModuleResponse;
+import net.fexcraft.mod.landdev.ui.LDUIModule;
+import net.fexcraft.mod.landdev.ui.modules.ModuleRequest;
+import net.fexcraft.mod.landdev.ui.modules.ModuleResponse;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
 public interface LDGuiSubModule {
 
-	public boolean sync_packet(LDGuiModule root, LDGuiContainer container, ModuleResponse resp);
+	public boolean sync_packet(LDUIModule root, LDGuiContainer container, ModuleResponse resp);
 
-	public boolean on_interact(LDGuiModule root, LDGuiContainer container, ModuleRequest req);
+	public boolean on_interact(LDUIModule root, LDGuiContainer container, ModuleRequest req);
 
 }
