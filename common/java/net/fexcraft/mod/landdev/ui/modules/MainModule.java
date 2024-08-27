11f461c99872df6cc9299520a576157fc8c4feb3
Ferdinand Calo <fexcalo@gmail.com>
2024-08-27 15:41:19 +0200

    Update MainModule.java

--- common/java/net/fexcraft/mod/landdev/ui/modules/MainModule.java
+++ common/java/net/fexcraft/mod/landdev/ui/modules/MainModule.java
@@ -1,12 +1,13 @@
 package net.fexcraft.mod.landdev.ui.modules;
 
 import static net.fexcraft.mod.landdev.ui.LDKeys.*;
+import static net.fexcraft.mod.landdev.ui.LDKeys.KEY_COUNTY;
 import static net.fexcraft.mod.landdev.ui.LDUIButton.OPEN;
 import static net.fexcraft.mod.landdev.ui.LDUIElmType.ELM_GENERIC;
 
 import net.fexcraft.mod.landdev.data.Layers;
 import net.fexcraft.mod.landdev.data.chunk.Chunk_;
-import net.fexcraft.mod.landdev.gui.LDGuiContainer;
+import net.fexcraft.mod.landdev.ui.BaseCon;
 import net.fexcraft.mod.landdev.ui.LDUIModule;
 import net.fexcraft.mod.landdev.util.ResManager;
 import net.fexcraft.mod.landdev.util.TranslationUtil;
@@ -16,7 +17,7 @@ public class MainModule implements LDUIModule {
 	public static MainModule INST = new MainModule();
 
 	@Override
-	public void sync_packet(LDGuiContainer container, ModuleResponse resp){
+	public void sync_packet(BaseCon container, ModuleResponse resp){
 		resp.setTitle("main.title");
 		resp.addButton("player", ELM_GENERIC, OPEN);
 		resp.addButton("mail", ELM_GENERIC, OPEN);
@@ -30,18 +31,18 @@ public class MainModule implements LDUIModule {
 		resp.addButton("state", ELM_GENERIC, OPEN);
 	}
 
-	public void on_interact(LDGuiContainer container, ModuleRequest req){
-		Chunk_ chunk = ResManager.getChunk(container.y, container.z);
+	public void on_interact(BaseCon container, ModuleRequest req){
+		Chunk_ chunk = ResManager.getChunk(container.pos.y, container.pos.z);
 		switch(req.event()){
-			case "player": container.open(PLAYER, 0, 0, 0); return;
-			case "mail": container.open(MAILBOX, Layers.PLAYER.ordinal(), 0, 0); return;
-			case "property": container.open(PROPERTY, 0, 0, 0); return;
-			case "company": container.open(COMPANY, 0, 0, 0); return;
-			case "chunk": container.open(CHUNK, 0, chunk.key.x, chunk.key.z); return;
-			case "district": container.open(DISTRICT, 0, chunk.district.id, 0); return;
+			case "player": container.open(KEY_PLAYER, 0, 0, 0); return;
+			case "mail": container.open(KEY_MAILBOX, Layers.PLAYER.ordinal(), 0, 0); return;
+			case "property": container.open(KEY_PROPERTY, 0, 0, 0); return;
+			case "company": container.open(KEY_COMPANY, 0, 0, 0); return;
+			case "chunk": container.open(KEY_CHUNK, 0, chunk.key.x, chunk.key.z); return;
+			case "district": container.open(KEY_DISTRICT, 0, chunk.district.id, 0); return;
 			case "municipality":{
 				if(!chunk.district.owner.is_county){
-					container.open(MUNICIPALITY, 0, chunk.district.owner.municipality.id, 0);
+					container.open(KEY_MUNICIPALITY, 0, chunk.district.owner.municipality.id, 0);
 				}
 				else{
 					container.player.entity.send(TranslationUtil.translate("district.not_part_of_municipality"));
@@ -50,10 +51,10 @@ public class MainModule implements LDUIModule {
 				return;
 			}
 			case "county":{
-				container.open(COUNTY, 0, chunk.district.owner.county_id(), 0);
+				container.open(KEY_COUNTY, 0, chunk.district.owner.county_id(), 0);
 				return;
 			}
-			case "state": container.open(STATE, 0, chunk.district.state().id, 0); return;
+			case "state": container.open(KEY_STATE, 0, chunk.district.state().id, 0); return;
 		}
 	}
 
