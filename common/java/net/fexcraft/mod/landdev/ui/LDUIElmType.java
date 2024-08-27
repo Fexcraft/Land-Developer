7b2e05095c36b6bcfd79ea8414b1f696a11f5132
Ferdinand Calo <fexcalo@gmail.com>
2024-08-27 11:02:36 +0200

    Update LDUIElmType.java

--- common/java/net/fexcraft/mod/landdev/ui/LDUIElmType.java
+++ common/java/net/fexcraft/mod/landdev/ui/LDUIElmType.java
@@ -11,20 +11,6 @@ public enum LDUIElmType {
 	ELM_EMPTY,
 	ELM_BLANK,
 	ELM_YELLOW,
-	ICON_ADD,
-	ICON_REM,
-	ICON_OPEN,
-	ICON_UP,
-	ICON_DOWN,
-	ICON_BLANK,
-	ICON_LIST,
-	ICON_EMPTY,
-	ICON_ENABLED,
-	ICON_DISABLED,
-	ICON_CHECKBOX_CHECKED,
-	ICON_CHECKBOX_UNCHECKED,
-	ICON_RADIOBOX_CHECKED,
-	ICON_RADIOBOX_UNCHECKED,
 	SCROLLBAR,
 	ICONBAR,
 	ICONBARCOLOR,
@@ -33,39 +19,7 @@ public enum LDUIElmType {
 	GO_BACK,
 	;
 	
-	public int x, y, w, h, px;
-
-	boolean icon(){
-		return ordinal() >= 9 && ordinal() < 23;
-	}
-
-	public static LDUIElmType checkbox(boolean bool){
-		return bool ? ICON_CHECKBOX_CHECKED : ICON_CHECKBOX_UNCHECKED;
-	}
-
-	public static LDUIElmType radio(boolean bool){
-		return bool ? ICON_RADIOBOX_CHECKED : ICON_RADIOBOX_UNCHECKED;
-	}
-
-	public static LDUIElmType enabled(boolean bool){
-		return bool ? ICON_ENABLED : ICON_DISABLED;
-	}
-
-	public boolean is_checkbox(){
-		return this == ICON_CHECKBOX_CHECKED || this == ICON_CHECKBOX_UNCHECKED;
-	}
-
-	public boolean is_radiobox(){
-		return this == ICON_RADIOBOX_CHECKED || this == ICON_RADIOBOX_UNCHECKED;
-	}
-
-	public boolean checkbox(){
-		return this == ICON_CHECKBOX_CHECKED;
-	}
-
-	public boolean radiobox(){
-		return this == ICON_RADIOBOX_CHECKED;
-	}
+	public int x, y, w, h;
 
 	public String translation(){
 		return "landdev.gui." + name().toLowerCase();
