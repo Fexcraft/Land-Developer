72daea3b8b4f56be5c1fccb578292cab13146737
Ferdinand Calo <fexcalo@gmail.com>
2024-08-27 15:41:15 +0200

    Update LDUIModule.java

--- common/java/net/fexcraft/mod/landdev/ui/LDUIModule.java
+++ common/java/net/fexcraft/mod/landdev/ui/LDUIModule.java
@@ -1,6 +1,5 @@
 package net.fexcraft.mod.landdev.ui;
 
-import net.fexcraft.mod.landdev.gui.LDGuiContainer;
 import net.fexcraft.mod.landdev.ui.modules.ModuleRequest;
 import net.fexcraft.mod.landdev.ui.modules.ModuleResponse;
 
@@ -12,17 +11,17 @@ public interface LDUIModule {
 	public static final String LANG_YES = "landdev.gui.yes", LANG_NO = "landdev.gui.no", VALONLY = "!!!";
 	public static final int UI_MAIN = 0;
 
-	public void sync_packet(LDGuiContainer container, ModuleResponse resp);
+	public void sync_packet(BaseCon container, ModuleResponse resp);
 
-	public void on_interact(LDGuiContainer container, ModuleRequest req);
+	public void on_interact(BaseCon container, ModuleRequest req);
 	
-	public default boolean validateName(LDGuiContainer container, String name){
+	public default boolean validateName(BaseCon container, String name){
 		if(name.length() < 1){
-			container.sendMsg("landdev.cmd.name_too_short", false);
+			container.msg("landdev.cmd.name_too_short", false);
 			return false;
 		}
 		if(name.length() > 32){
-			container.sendMsg("landdev.cmd.name_too_long", false);
+			container.msg("landdev.cmd.name_too_long", false);
 			return false;
 		}
 		return true;
@@ -33,11 +32,11 @@ public interface LDUIModule {
 		public static Missing INST = new Missing();
 
 		@Override
-		public void sync_packet(LDGuiContainer container, ModuleResponse resp){
+		public void sync_packet(BaseCon container, ModuleResponse resp){
 			resp.setTitle("missing.title");
 		}
 
-		public void on_interact(LDGuiContainer container, ModuleRequest req){
+		public void on_interact(BaseCon container, ModuleRequest req){
 			//
 		}
 
