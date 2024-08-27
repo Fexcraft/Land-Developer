402accaa3cb3b96059305e385946ccf9b382ae95
Ferdinand Calo <fexcalo@gmail.com>
2024-08-27 14:36:32 +0200

    Create BaseUI.java

---
+++ common/java/net/fexcraft/mod/landdev/ui/BaseUI.java
@@ -0,0 +1,16 @@
+package net.fexcraft.mod.landdev.ui;
+
+import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.uni.ui.ContainerInterface;
+import net.fexcraft.mod.uni.ui.UserInterface;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class BaseUI extends UserInterface {
+
+	public BaseUI(JsonMap map, ContainerInterface container) throws Exception {
+		super(map, container);
+	}
+
+}
