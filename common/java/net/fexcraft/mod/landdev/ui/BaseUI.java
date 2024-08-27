b19ec8e1c1d580de9ed00cc4965e640f7e84acb3
Ferdinand Calo <fexcalo@gmail.com>
2024-08-27 15:41:10 +0200

    Update BaseUI.java

--- common/java/net/fexcraft/mod/landdev/ui/BaseUI.java
+++ common/java/net/fexcraft/mod/landdev/ui/BaseUI.java
@@ -1,6 +1,7 @@
 package net.fexcraft.mod.landdev.ui;
 
 import net.fexcraft.app.json.JsonMap;
+import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.ui.ContainerInterface;
 import net.fexcraft.mod.uni.ui.UserInterface;
 
@@ -11,6 +12,13 @@ public class BaseUI extends UserInterface {
 
 	public BaseUI(JsonMap map, ContainerInterface container) throws Exception {
 		super(map, container);
+		sync();
+	}
+
+	private void sync(){
+		TagCW com = TagCW.create();
+		com.set("sync", true);
+		container.SEND_TO_SERVER.accept(com);
 	}
 
 }
