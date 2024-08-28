f92ea964e93e6d297de9a2f2a3ab83ac1b3650d0
Ferdinand Calo <fexcalo@gmail.com>
2024-08-28 19:09:48 +0200

    Update LDKeys.java

--- common/java/net/fexcraft/mod/landdev/ui/LDKeys.java
+++ common/java/net/fexcraft/mod/landdev/ui/LDKeys.java
@@ -2,6 +2,9 @@ package net.fexcraft.mod.landdev.ui;
 
 import net.fexcraft.mod.uni.ui.UIKey;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
@@ -22,7 +25,6 @@ public class LDKeys {
 	public static final int CLAIM = 100;
 	public static final int IMG_PREVIEW = 200;
 
-
 	public static final UIKey KEY_MAIN = new UIKey(MAIN, "landdev:main");
 	public static final UIKey KEY_PROPERTY = new UIKey(PROPERTY, "landdev:property");
 	public static final UIKey KEY_CHUNK = new UIKey(CHUNK, "landdev:chunk");
@@ -37,5 +39,20 @@ public class LDKeys {
 	public static final UIKey KEY_MAIL = new UIKey(MAIN, "landdev:mail");
 	public static final UIKey KEY_CLAIM = new UIKey(MAIN, "landdev:claim");
 	public static final UIKey KEY_IMG_VIEW = new UIKey(MAIN, "landdev:img_view");
+
+	public static final HashMap<UIKey, Class<? extends BaseCon>> CONS = new LinkedHashMap<>();
+	static{
+		CONS.put(KEY_MAIN, BaseCon.class);
+		CONS.put(KEY_PROPERTY, BaseCon.PropBaseCon.class);
+		CONS.put(KEY_CHUNK, BaseCon.ChunkBaseCon.class);
+		CONS.put(KEY_DISTRICT, BaseCon.DisBaseCon.class);
+		CONS.put(KEY_MUNICIPALITY, BaseCon.MunBaseCon.class);
+		CONS.put(KEY_COUNTY, BaseCon.CouBaseCon.class);
+		CONS.put(KEY_STATE, BaseCon.StaBaseCon.class);
+		CONS.put(KEY_PLAYER, BaseCon.PlayerBaseCon.class);
+		CONS.put(KEY_POLL, BaseCon.PollBaseCon.class);
+		CONS.put(KEY_MAILBOX, BaseCon.MBBaseCon.class);
+		CONS.put(KEY_MAIL, BaseCon.MailBaseCon.class);
+	}
 
 }
