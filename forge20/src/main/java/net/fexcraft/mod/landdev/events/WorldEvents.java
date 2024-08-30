b9444ae92e52be5717afc4bafcaafbc893484fca
Ferdinand Calo <fexcalo@gmail.com>
2024-08-30 16:02:12 +0200

    Create WorldEvents.java

---
+++ forge20/src/main/java/net/fexcraft/mod/landdev/events/WorldEvents.java
@@ -0,0 +1,33 @@
+package net.fexcraft.mod.landdev.events;
+
+import net.fexcraft.mod.fsmm.FSMM;
+import net.fexcraft.mod.landdev.LandDev;
+import net.fexcraft.mod.landdev.util.ResManager;
+import net.minecraftforge.event.level.LevelEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.server.ServerLifecycleHooks;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+@Mod.EventBusSubscriber(modid = "landdev", bus = Mod.EventBusSubscriber.Bus.FORGE)
+public class WorldEvents {
+	
+	@SubscribeEvent(priority = EventPriority.HIGHEST)
+	public static void onWorldLoad(LevelEvent.Load event){
+		if(event.getLevel().isClientSide()) return;
+		if(event.getLevel() != ServerLifecycleHooks.getCurrentServer().overworld()) return;
+	}
+	
+	@SubscribeEvent(priority = EventPriority.LOWEST)
+	public static void onWorldUnload(LevelEvent.Unload event){
+		if(event.getLevel().isClientSide()) return;
+		if(event.getLevel() != ServerLifecycleHooks.getCurrentServer().overworld()) return;
+		LandDev.log("Unloading LandDev World Data...");
+		ResManager.unload();
+		ResManager.clear();
+	}
+	
+}
