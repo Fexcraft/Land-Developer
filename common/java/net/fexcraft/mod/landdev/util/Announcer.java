3c6c3db3eef362f4fc8552434d3720c80fe1d5de
Ferdinand Calo <fexcalo@gmail.com>
2024-08-30 16:01:55 +0200

    Update Announcer.java

--- common/java/net/fexcraft/mod/landdev/util/Announcer.java
+++ common/java/net/fexcraft/mod/landdev/util/Announcer.java
@@ -1,8 +1,5 @@
 package net.fexcraft.mod.landdev.util;
 
-import net.fexcraft.lib.mc.utils.Static;
-import net.fexcraft.mod.landdev.data.player.LDPlayer;
-import net.fexcraft.mod.uni.UniEntity;
 import net.fexcraft.mod.uni.world.MessageSender;
 
 public class Announcer {
@@ -10,35 +7,35 @@ public class Announcer {
 	public static void announce(Target target, int id, String string, Object... objs){
 		switch(target){
 			case GLOBAL:
-				Static.getServer().getPlayerList().getPlayers().forEach(player -> { announce(UniEntity.getEntity(player), string, objs); });
+				//TODO Static.getServer().getPlayerList().getPlayers().forEach(player -> { announce(UniEntity.getEntity(player), string, objs); });
 				break;
 			case LOCAL:
 				break;
 			case COMPANY:
 				break;
 			case DISTRICT:
-				Static.getServer().getPlayerList().getPlayers().forEach(player -> {
+				/*Static.getServer().getPlayerList().getPlayers().forEach(player -> {
 					LDPlayer ply = ResManager.getPlayer(player.getGameProfile().getId(), true);
 					if(ply.isCurrentlyInDistrict(id)) announce(ply.entity, string, objs);
-				});
+				});*/
 				break;
 			case MUNICIPALITY:
-				Static.getServer().getPlayerList().getPlayers().forEach(player -> {
+				/*Static.getServer().getPlayerList().getPlayers().forEach(player -> {
 					LDPlayer ply = ResManager.getPlayer(player.getGameProfile().getId(), true);
 					if(ply.municipality.id == id || ply.isCurrentlyInMunicipality(id)) announce(ply.entity, string, objs);
-				});
+				});*/
 				break;
 			case COUNTY:
-				Static.getServer().getPlayerList().getPlayers().forEach(player -> {
+				/*Static.getServer().getPlayerList().getPlayers().forEach(player -> {
 					LDPlayer ply = ResManager.getPlayer(player.getGameProfile().getId(), true);
 					if(ply.county.id == id || ply.isCurrentlyInCounty(id)) announce(ply.entity, string, objs);
-				});
+				});*/
 				break;
 			case STATE:
-				Static.getServer().getPlayerList().getPlayers().forEach(player -> {
+				/*Static.getServer().getPlayerList().getPlayers().forEach(player -> {
 					LDPlayer ply = ResManager.getPlayer(player.getGameProfile().getId(), true);
 					if(ply.county.state.id == id || ply.isCurrentlyInState(id)) announce(ply.entity, string, objs);
-				});
+				});*/
 				break;
 			default: return;
 		}
