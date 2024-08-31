854531966b1e60b30c302e3b2d3c7a90ab12d967
Ferdinand Calo <fexcalo@gmail.com>
2024-08-31 11:51:02 +0200

    Update Protector.java

--- common/java/net/fexcraft/mod/landdev/util/Protector.java
+++ common/java/net/fexcraft/mod/landdev/util/Protector.java
@@ -1,36 +1,22 @@
 package net.fexcraft.mod.landdev.util;
 
 import java.io.File;
-import java.util.ArrayList;
 
 import net.fexcraft.app.json.JsonArray;
 import net.fexcraft.app.json.JsonHandler;
 import net.fexcraft.app.json.JsonHandler.PrintOption;
 import net.fexcraft.app.json.JsonMap;
-import net.minecraft.block.Block;
-import net.minecraft.block.state.IBlockState;
 
 public class Protector {
 	
-	public static Instance INSTANCE;
+	public static ProtectorInstance INSTANCE;
 	
 	public static void load(){
-		File file = new File(LDConfig.CONFIG_PATH, "landdev-interaction.json");
+		File file = new File(LDConfig.CONFIG_PATH, "landdev-protector.json");
 		if(!file.exists()) generate(file);
 		JsonMap map = JsonHandler.parse(file);
 		boolean abs = map.getBoolean("absolute_block_protection", false);
-		if(abs){
-			INSTANCE = new Instance(null){
-				@Override
-				public boolean isProtected(IBlockState state){
-					return true;
-				}
-			};
-			return;
-		}
-		if(map.has("blocks")){
-			Protector.INSTANCE = new Instance(map.getArray("blocks"));
-		}
+		Protector.INSTANCE = ProtectorInstance.get(abs, map);
 	}
 
 	private static void generate(File file){
@@ -38,6 +24,7 @@ public class Protector {
 		map.add("__comment1", "In this file you can define custom interaction protection, by adding an entry the block becomes protected.");
 		map.add("__comment2", "If you set 'absolute_block_protection' to true, the list in this file is ignored and all blocks become protected.");
 		map.add("__comment3", "Note: There is a hardcoded exception for SIGN blocks.");
+		map.add("absolute_block_protection", false);
 		JsonArray blocks = new JsonArray();
 		blocks.add("minecraft:chest");
 		blocks.add("minecraft:ender_chest");
@@ -53,23 +40,6 @@ public class Protector {
 		JsonArray entities = new JsonArray();
 		map.add("entities", entities);
 		JsonHandler.print(file, map, PrintOption.SPACED);
-	}
-	
-	public static class Instance {
-		
-		private ArrayList<Block> blocks = new ArrayList<>();
-
-		public Instance(JsonArray array){
-			array.value.forEach(elm -> {
-				Block block = Block.getBlockFromName(elm.string_value());
-				if(block != null) blocks.add(block);
-			});
-		}
-
-		public boolean isProtected(IBlockState state){
-			return blocks.contains(state.getBlock());
-		}
-		
 	}
 
 }
