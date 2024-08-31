272f15d318ea8bc01876f4f3e7a9fe446a6e919a
Ferdinand Calo <fexcalo@gmail.com>
2024-08-31 11:51:27 +0200

    Update TranslationUtil.java

--- forge20/src/main/java/net/fexcraft/mod/landdev/util/TranslationUtil.java
+++ forge20/src/main/java/net/fexcraft/mod/landdev/util/TranslationUtil.java
@@ -3,11 +3,11 @@ package net.fexcraft.mod.landdev.util;
 public class TranslationUtil {
 
 	public static String translate(String str, Object... args){
-		return str;
+		return "landdev." + str;
 	}
 
-	public static String translateCmd(String str){
-		return str;
+	public static String translateCmd(String str, Object... args){
+		return "landdev.cmd." + str;
 	}
 
 }
