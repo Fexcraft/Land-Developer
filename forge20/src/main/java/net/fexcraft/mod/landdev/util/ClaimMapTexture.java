0c5d15eef8c13ec1f54380460bc81da75e3fdcb5
Ferdinand Calo <fexcalo@gmail.com>
2024-08-31 16:05:09 +0200

    Create ClaimMapTexture.java

---
+++ forge20/src/main/java/net/fexcraft/mod/landdev/util/ClaimMapTexture.java
@@ -0,0 +1,18 @@
+package net.fexcraft.mod.landdev.util;
+
+import net.fexcraft.mod.landdev.ui.ChunkClaimUI;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class ClaimMapTexture {
+
+	public static void bind(ChunkClaimUI ui, int x, int z){
+
+	}
+
+	public static void delete(){
+
+	}
+
+}
