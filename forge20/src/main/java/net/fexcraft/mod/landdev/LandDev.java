ae35028662a3180429f594175a04001269bb0b86
Ferdinand Calo <fexcalo@gmail.com>
2024-09-01 17:47:50 +0200

    Update LandDev.java

--- forge20/src/main/java/net/fexcraft/mod/landdev/LandDev.java
+++ forge20/src/main/java/net/fexcraft/mod/landdev/LandDev.java
@@ -3,17 +3,28 @@ package net.fexcraft.mod.landdev;
 import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.logging.LogUtils;
+import net.fexcraft.lib.common.math.Time;
 import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.fcl.util.ClientPacketPlayer;
+import net.fexcraft.mod.fcl.util.UIPacketF;
 import net.fexcraft.mod.landdev.data.chunk.Chunk_;
+import net.fexcraft.mod.landdev.data.county.County;
+import net.fexcraft.mod.landdev.data.municipality.Municipality;
 import net.fexcraft.mod.landdev.data.player.LDPlayer;
+import net.fexcraft.mod.landdev.events.LocationUpdate;
 import net.fexcraft.mod.landdev.ui.LDKeys;
-import net.fexcraft.mod.landdev.util.AliasLoader;
 import net.fexcraft.mod.landdev.util.ResManager;
 import net.fexcraft.mod.landdev.util.TranslationUtil;
 import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.world.EntityW;
 import net.minecraft.commands.CommandSourceStack;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.nbt.NbtIo;
 import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.level.Level;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
@@ -29,12 +40,16 @@ import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.network.NetworkRegistry;
+import net.minecraftforge.network.PacketDistributor;
+import net.minecraftforge.network.simple.SimpleChannel;
 import org.slf4j.Logger;
 
 import java.io.File;
 import java.io.IOException;
 
 import static net.fexcraft.mod.fsmm.local.FsmmCmd.isOp;
+import static net.fexcraft.mod.landdev.data.PermAction.CREATE_MUNICIPALITY;
 import static net.fexcraft.mod.landdev.util.TranslationUtil.translateCmd;
 import static net.minecraft.commands.Commands.argument;
 import static net.minecraft.commands.Commands.literal;
@@ -46,6 +61,11 @@ public class LandDev {
 	public static final String VERSION = "1.2.0";
 	private static final Logger LOGGER = LogUtils.getLogger();
 	public static File SAVE_DIR;
+	public static final SimpleChannel CHANNEL = NetworkRegistry.ChannelBuilder.named(new ResourceLocation("landdev", "channel"))
+		.clientAcceptedVersions(pro -> true)
+		.serverAcceptedVersions(pro -> true)
+		.networkProtocolVersion(() -> VERSION)
+		.simpleChannel();
 
 	public LandDev(){
 		IEventBus modbus = FMLJavaModLoadingContext.get().getModEventBus();
@@ -57,6 +77,22 @@ public class LandDev {
 	private void commonSetup(final FMLCommonSetupEvent event){
 		LDN.init(this);
 		LDN.postinit();
+		CHANNEL.registerMessage(1, UIPacketF.class, (packet, buffer) -> buffer.writeNbt(packet.com()), buffer -> new UIPacketF(buffer.readNbt()), (packet, context) -> {
+			context.get().enqueueWork(() -> {
+				if(context.get().getDirection().getOriginationSide().isClient()){
+					ServerPlayer player = context.get().getSender();
+					onServerPacket(player, packet.com());
+				}
+				else{
+					ClientModEvents.onClientPacket(ClientPacketPlayer.get(), packet.com());
+				}
+			});
+			context.get().setPacketHandled(true);
+		});
+	}
+
+	private static void onServerPacket(ServerPlayer player, CompoundTag com){
+
 	}
 
 	public static TagCW read(File file) throws IOException {
@@ -103,12 +139,27 @@ public class LandDev {
 			//
 		}
 
+		private static void onClientPacket(Player player, CompoundTag com){
+			switch(com.getString("task")){
+				case "location":{
+					int time = com.contains("time") ? com.getInt("time") : 10;
+					LocationUpdate.clear(Time.getDate() + (time * 1000));
+					LocationUpdate.loadIcons((ListTag)com.get("icons"));
+					LocationUpdate.loadLines((ListTag)com.get("lines"));
+				}
+			}
+		}
+
 	}
 
 	@SubscribeEvent
 	public void onCmdReg(RegisterCommandsEvent event){
 		//AliasLoader.load();
 		regCmd(event.getDispatcher());
+	}
+
+	public static void sendLocationPacket(EntityW entity, TagCW com){
+		CHANNEL.send(PacketDistributor.PLAYER.with(() -> entity.local()), new UIPacketF(com.local()));
 	}
 
 	private void regCmd(CommandDispatcher<CommandSourceStack> dispatcher){
@@ -179,7 +230,17 @@ public class LandDev {
 		dispatcher.register(literal("mun")
 			.then(literal("create").executes(cmd -> {
 				LDPlayer player = ResManager.getPlayer(cmd.getSource().getPlayer());
-				//
+				Chunk_ chunk = ResManager.getChunk(player.entity);
+				County county = chunk.district.county();
+				boolean cn = county.norms.get("new-municipalities").bool();
+				boolean pp = player.hasPermit(CREATE_MUNICIPALITY, county.getLayer(), county.id);
+				if(!cn && !pp){
+					player.entity.send(translateCmd("mun.no_new_municipalities"));
+					player.entity.send(translateCmd("mun.no_create_permit"));
+				}
+				else{
+					player.entity.openUI(LDKeys.KEY_MUNICIPALITY, Municipality.UI_CREATE, 0, 0);
+				}
 				return 0;
 			}))
 			.executes(cmd -> {
