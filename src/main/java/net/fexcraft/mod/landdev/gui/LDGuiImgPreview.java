7984e5483d5f2cff932000b3a161c45c3b78c29e
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-13 22:02:42 +0100

    Update LDGuiImgPreview.java

--- src/main/java/net/fexcraft/mod/landdev/gui/LDGuiImgPreview.java
+++ src/main/java/net/fexcraft/mod/landdev/gui/LDGuiImgPreview.java
@@ -2,7 +2,6 @@ package net.fexcraft.mod.landdev.gui;
 
 import net.fexcraft.lib.mc.gui.GenericContainer;
 import net.fexcraft.lib.mc.gui.GenericGui;
-import net.fexcraft.lib.mc.registry.UCResourceLocation;
 import net.minecraft.client.renderer.BufferBuilder;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
@@ -12,7 +11,7 @@ import net.minecraft.util.ResourceLocation;
 public class LDGuiImgPreview extends GenericGui<GenericContainer> {
 	
 	private static final ResourceLocation TEXTURE = new ResourceLocation("minecraft:textures/blocks/stone.png");
-	public static UCResourceLocation IMG_URL;
+	public static ResourceLocation IMG_URL;
 	private int imgw, imgh;
 
 	public LDGuiImgPreview(EntityPlayer player, int x, int y, int z){
