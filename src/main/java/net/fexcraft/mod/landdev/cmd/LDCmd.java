586a7822986e00fbded79a8a6c72091a3b27f373
Ferdinand Calo <fercalo96@yahoo.de>
2025-01-16 22:31:11 +0100

    Update LDCmd.java

--- src/main/java/net/fexcraft/mod/landdev/cmd/LDCmd.java
+++ src/main/java/net/fexcraft/mod/landdev/cmd/LDCmd.java
@@ -95,7 +95,7 @@ public class LDCmd extends CommandBase {
 	    		case "img":{
 	    			int w = args.length > 2 ? Integer.parseInt(args[2]) : 256;
 	    			int h = args.length > 3 ? Integer.parseInt(args[3]) : 256;
-	    			player.openGui(LDKeys.IMG_PREVIEW, w, h, 0);
+	    			player.entity.openUI(LDKeys.KEY_IMG_VIEW, w, h, 0);
 	    			NBTTagCompound com = new NBTTagCompound();
 	    			com.setString("target_listener", CLIENT_RECEIVER_ID);
 	    			com.setString("task", "img_preview_url");
