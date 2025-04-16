065b431707484f8c33fd7c455f78186e57b8ce37
Ferdinand Calo <fexcalo@gmail.com>
2025-04-16 21:33:10 +0200

    Update InteractionEvents.java

--- forge20/src/main/java/net/fexcraft/mod/landdev/events/InteractionEvents.java
+++ forge20/src/main/java/net/fexcraft/mod/landdev/events/InteractionEvents.java
@@ -21,6 +21,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.server.ServerLifecycleHooks;
 
+import static net.fexcraft.mod.landdev.util.InteractHandler.control;
 import static net.fexcraft.mod.landdev.util.TranslationUtil.translate;
 
 /**
@@ -33,11 +34,10 @@ public class InteractionEvents {
 	public static void onBlockBreak0(BlockEvent.BreakEvent event){
 		if(event.getLevel().isClientSide()) return;
 		if(event.getLevel() != ServerLifecycleHooks.getCurrentServer().overworld()) return;
-		if(!control((Level)event.getLevel(), event.getPos(), event.getState(), event.getPlayer(), false)){
-			UniEntity.getEntity(event.getPlayer()).bar(translate("interact.break.noperm"));
+		if(!control(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getPlayer(), false)){
+			UniEntity.getEntity(event.getPlayer()).bar("interact.break.noperm");
 			event.setCanceled(true);
 		}
-		return;
 	}
 
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
@@ -45,11 +45,10 @@ public class InteractionEvents {
 		if(event.getLevel().isClientSide()) return;
 		if(event.getEntity() instanceof Player == false) return;
 		if(event.getLevel() != ServerLifecycleHooks.getCurrentServer().overworld()) return;
-		if(!control((Level)event.getLevel(), event.getPos(), event.getState(), (Player)event.getEntity(), false)){
-			UniEntity.getEntity(event.getEntity()).bar(translate("interact.place.noperm"));
+		if(!control(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getEntity(), false)){
+			UniEntity.getEntity(event.getEntity()).bar("interact.place.noperm");
 			event.setCanceled(true);
 		}
-		return;
 	}
 
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
@@ -57,11 +56,10 @@ public class InteractionEvents {
 		if(event.getLevel().isClientSide()) return;
 		if(event.getEntity() instanceof Player == false) return;
 		if(event.getLevel() != ServerLifecycleHooks.getCurrentServer().overworld()) return;
-		if(!control((Level)event.getLevel(), event.getPos(), event.getState(), (Player)event.getEntity(), false)){
-			UniEntity.getEntity(event.getEntity()).bar(translate("interact.place.noperm"));
+		if(!control(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getEntity(), false)){
+			UniEntity.getEntity(event.getEntity()).bar("interact.place.noperm");
 			event.setCanceled(true);
 		}
-		return;
 	}
 	
 	@SubscribeEvent
@@ -71,107 +69,10 @@ public class InteractionEvents {
 		if(event.getHand() == InteractionHand.OFF_HAND) return;
 		BlockState state = event.getLevel().getBlockState(event.getPos());
 		boolean check = state.getBlock() instanceof SignBlock == false && Protector.INSTANCE.isProtected(state);
-		if(check && !control(event.getLevel(), event.getPos(), state, event.getEntity(), true)){
-			UniEntity.getEntity(event.getEntity()).bar(translate("interact.interact.noperm"));
+		if(check && !control(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getEntity(), true)){
+			UniEntity.getEntity(event.getEntity()).bar("interact.interact.noperm");
 			event.setCanceled(true);
 		}
-	}
-
-	private static boolean control(Level world, BlockPos pos, BlockState state, Player entity, boolean interact){
-		LDPlayer player = ResManager.getPlayer(entity);
-		if(player == null) return false;
-		if(player.adm) return true;
-		Chunk_ chunk = ResManager.getChunkS(pos.getX(), pos.getZ());
-		if(chunk.district.id < 0){
-			if(chunk.district.id == -1){
-				return LDConfig.EDIT_WILDERNESS;
-			}
-			else if(chunk.district.id == -2){
-				//TODO temporary claims
-				return false;
-			}
-			else{
-				player.entity.bar(translate("interact.control.unknown_district"));
-				return false;
-			}
-		}
-		return hasperm(chunk, player, interact);
-	}
-
-	private static boolean hasperm(Chunk_ chunk, LDPlayer player, boolean interact){
-		if(interact && chunk.access.interact) return true;
-		ChunkType type = chunk.owner.unowned ? ChunkType.NORMAL : chunk.type;
-		boolean pass = false;
-		switch(type){
-		case NORMAL:
-			pass = chunk.district.owner.isPartOf(player);
-			break;
-		case PRIVATE:
-			if(chunk.owner.playerchunk){
-				pass = player.uuid.equals(chunk.owner.player);
-			}
-			else{
-				switch(chunk.owner.owner){
-				case COMPANY:
-					//TODO
-					break;
-				case COUNTY:
-					pass = chunk.district.owner.manageable().isManager(player.uuid);
-					break;
-				case DISTRICT:
-					pass = chunk.district.manage.isManager(player.uuid) || chunk.district.owner.manageable().isManager(player.uuid);
-					break;
-				case MUNICIPALITY:
-					pass = chunk.district.owner.manageable().isManager(player.uuid);
-					break;
-				case REGION:
-					pass = chunk.district.region().manage.isManager(player.uuid);
-					break;
-				default:
-					break;
-				}
-			}
-			break;
-		case PUBLIC:
-			pass = true;
-			break;
-		case RESTRICTED:
-			switch(chunk.owner.owner){
-			case COMPANY:
-				//TODO
-				break;
-			case COUNTY:
-				pass = chunk.district.owner.manageable().isStaff(player.uuid);
-				break;
-			case DISTRICT:
-				pass = chunk.district.manage.isManager(player.uuid) || chunk.district.owner.manageable().isStaff(player.uuid);
-				break;
-			case MUNICIPALITY:
-				pass = chunk.district.owner.manageable().isStaff(player.uuid);
-				break;
-			case REGION:
-				pass = chunk.district.region().manage.isStaff(player.uuid);
-				break;
-			default:
-				break;
-			}
-			break;
-		default:
-			player.entity.bar(translate("interact.control.unknown_chunk_type"));
-			return false;
-		
-		}
-		if(!pass){
-			Long l = chunk.access.players.get(player.uuid);
-			if(l != null){
-				if(Time.getDate() < l) return true;
-				else{
-					chunk.access.players.remove(player.uuid);
-				}
-			}
-			//TODO company check
-		}
-		return pass;
 	}
 
 }
