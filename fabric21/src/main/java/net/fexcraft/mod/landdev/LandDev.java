d47767cd2d1819ed996decabc0ccc7e81e3c95ad
Ferdinand Calo <fexcalo@gmail.com>
2025-04-27 13:56:42 +0200

    Update LandDev.java

--- fabric21/src/main/java/net/fexcraft/mod/landdev/LandDev.java
+++ fabric21/src/main/java/net/fexcraft/mod/landdev/LandDev.java
@@ -3,9 +3,15 @@ package net.fexcraft.mod.landdev;
 import net.fabricmc.api.ModInitializer;
 
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import net.fabricmc.loader.api.FabricLoader;
+import net.fexcraft.mod.fcl.FCL;
+import net.fexcraft.mod.fcl.util.TagPacket;
+import net.fexcraft.mod.fcl.util.UIPacket;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.world.EntityW;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.server.level.ServerPlayer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -26,10 +32,21 @@ public class LandDev implements ModInitializer {
 		ServerLifecycleEvents.SERVER_STARTED.register(server -> LDN.onServerStarted());
 		ServerLifecycleEvents.SERVER_STOPPING.register(server -> LDN.onServerStopping());
 		ServerLifecycleEvents.SERVER_STOPPED.register(server -> LDN.onServerStop());
+
+
+		FCL.addListener("landdev", false, (com, player) -> {
+
+		});
 	}
 
 	public static void sendLocationPacket(EntityW entity, TagCW com){
+		ServerPlayNetworking.getSender((ServerPlayer)entity.direct()).sendPacket(new TagPacket("landdev", com));
+	}
 
+	public static void sendToAll(TagCW com){
+		for(ServerPlayer player : FCL.SERVER.get().getPlayerList().getPlayers()){
+			ServerPlayNetworking.getSender(player).sendPacket(new TagPacket("landdev", com));
+		}
 	}
 
 }
