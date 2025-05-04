46c926d505fccad9eb60cc46d40d96788865aaf0
Ferdinand Calo <fexcalo@gmail.com>
2025-05-04 17:46:12 +0200

    Update InteractionEvents.java

--- forge20/src/main/java/net/fexcraft/mod/landdev/events/InteractionEvents.java
+++ forge20/src/main/java/net/fexcraft/mod/landdev/events/InteractionEvents.java
@@ -1,5 +1,6 @@
 package net.fexcraft.mod.landdev.events;
 
+import net.fexcraft.mod.fcl.FCL;
 import net.fexcraft.mod.landdev.util.Protector;
 import net.fexcraft.mod.uni.UniEntity;
 import net.minecraft.world.InteractionHand;
@@ -11,7 +12,6 @@ import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.server.ServerLifecycleHooks;
 
 import static net.fexcraft.mod.landdev.util.InteractHandler.control;
 
@@ -24,7 +24,7 @@ public class InteractionEvents {
 	@SubscribeEvent(priority = EventPriority.HIGHEST)
 	public static void onBlockBreak0(BlockEvent.BreakEvent event){
 		if(event.getLevel().isClientSide()) return;
-		if(event.getLevel() != ServerLifecycleHooks.getCurrentServer().overworld()) return;
+		if(event.getLevel() != FCL.SERVER.get().overworld()) return;
 		if(!control(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getPlayer(), false)){
 			UniEntity.getEntity(event.getPlayer()).bar("interact.break.noperm");
 			event.setCanceled(true);
@@ -35,7 +35,7 @@ public class InteractionEvents {
 	public static void onBlockPlace(BlockEvent.EntityPlaceEvent event){
 		if(event.getLevel().isClientSide()) return;
 		if(event.getEntity() instanceof Player == false) return;
-		if(event.getLevel() != ServerLifecycleHooks.getCurrentServer().overworld()) return;
+		if(event.getLevel() != FCL.SERVER.get().overworld()) return;
 		if(!control(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getEntity(), false)){
 			UniEntity.getEntity(event.getEntity()).bar("interact.place.noperm");
 			event.setCanceled(true);
@@ -46,7 +46,7 @@ public class InteractionEvents {
 	public static void onBlockPlace(BlockEvent.EntityMultiPlaceEvent event){
 		if(event.getLevel().isClientSide()) return;
 		if(event.getEntity() instanceof Player == false) return;
-		if(event.getLevel() != ServerLifecycleHooks.getCurrentServer().overworld()) return;
+		if(event.getLevel() != FCL.SERVER.get().overworld()) return;
 		if(!control(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getEntity(), false)){
 			UniEntity.getEntity(event.getEntity()).bar("interact.place.noperm");
 			event.setCanceled(true);
@@ -56,7 +56,7 @@ public class InteractionEvents {
 	@SubscribeEvent
 	public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event){
 		if(event.getLevel().isClientSide()) return;
-		if(event.getLevel() != ServerLifecycleHooks.getCurrentServer().overworld()) return;
+		if(event.getLevel() != FCL.SERVER.get().overworld()) return;
 		if(event.getHand() == InteractionHand.OFF_HAND) return;
 		BlockState state = event.getLevel().getBlockState(event.getPos());
 		boolean check = state.getBlock() instanceof SignBlock == false && Protector.INSTANCE.isProtected(state);
