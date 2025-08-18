4a4d9c6dc005a018d3ac06eadb8860d2efe798d9
Ferdinand Calo <fexcalo@gmail.com>
2025-08-18 23:33:36 +0200

    Update ChunkEvents.java

--- forge12/src/main/java/net/fexcraft/mod/landdev/events/ChunkEvents.java
+++ forge12/src/main/java/net/fexcraft/mod/landdev/events/ChunkEvents.java
@@ -1,14 +1,10 @@
 package net.fexcraft.mod.landdev.events;
 
-import static net.fexcraft.mod.landdev.util.broad.Broadcaster.TargetTransmitter.NO_INTERNAL;
-
 import net.fexcraft.mod.fsmm.FSMM;
 import net.fexcraft.mod.landdev.LandDev;
 import net.fexcraft.mod.landdev.data.chunk.Chunk_;
 import net.fexcraft.mod.landdev.util.ResManager;
-import net.fexcraft.mod.landdev.util.TranslationUtil;
-import net.fexcraft.mod.landdev.util.broad.BroadcastChannel;
-import net.fexcraft.mod.landdev.util.broad.Broadcaster;
+import net.fexcraft.mod.uni.world.WrapperHolder;
 import net.minecraft.world.World;
 import net.minecraftforge.event.world.ChunkEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -26,6 +22,7 @@ public class ChunkEvents {
 	private static void load(World world){
 		if(world.isRemote) return;
 		if(!FSMM.isDataManagerLoaded()) FSMM.loadDataManager();
+		LandDev.SAVE_DIR = WrapperHolder.getWorldFolder(WrapperHolder.getWorld(world), "landdev");
 		ResManager.INSTANCE.load();
 	}
 
