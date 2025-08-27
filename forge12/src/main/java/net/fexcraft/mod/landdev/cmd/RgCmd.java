0c6cb19cf92a63840e2dbfc932a74d37efb7c5f2
Ferdinand Calo <fexcalo@gmail.com>
2025-08-27 21:40:40 +0200

    Update RgCmd.java

--- forge12/src/main/java/net/fexcraft/mod/landdev/cmd/RgCmd.java
+++ forge12/src/main/java/net/fexcraft/mod/landdev/cmd/RgCmd.java
@@ -1,6 +1,5 @@
 package net.fexcraft.mod.landdev.cmd;
 
-import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.mod.landdev.data.chunk.Chunk_;
 import net.fexcraft.mod.landdev.data.county.County;
 import net.fexcraft.mod.landdev.data.player.LDPlayer;
@@ -15,8 +14,6 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.server.MinecraftServer;
 
 import java.util.List;
-
-import static net.fexcraft.mod.landdev.util.TranslationUtil.translateCmd;
 
 public class RgCmd extends CommandBase {
 
@@ -51,8 +48,8 @@ public class RgCmd extends CommandBase {
     		switch(args[0]){
 	    		case "create":{
 					if(!LDConfig.NEW_REGIONS && !ply.adm){
-						Print.chat(sender, translateCmd("rg.no_new_region"));
-						Print.chat(sender, translateCmd("rg.no_create_permit"));
+						ply.entity.send("landdev.cmd.rg.no_new_region");
+						ply.entity.send("landdev.cmd.rg.no_create_permit");
 					}
 					else{
 						ply.entity.openUI(LDKeys.REGION, County.UI_CREATE, 0, 0);
@@ -60,7 +57,7 @@ public class RgCmd extends CommandBase {
 	    			return;
 	    		}
 	    		default:{
-	    			Print.chat(sender, translateCmd("unknown_argument"));
+	    			ply.entity.send("landdev.cmd.unknown_argument");
 	    			return;
 	    		}
     		}
