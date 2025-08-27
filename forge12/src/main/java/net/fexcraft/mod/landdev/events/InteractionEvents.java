797feed06ab74cccdc3404ddf7d7567860266986
Ferdinand Calo <fexcalo@gmail.com>
2025-08-27 21:51:31 +0200

    Update InteractionEvents.java

--- forge12/src/main/java/net/fexcraft/mod/landdev/events/InteractionEvents.java
+++ forge12/src/main/java/net/fexcraft/mod/landdev/events/InteractionEvents.java
@@ -1,23 +1,12 @@
 package net.fexcraft.mod.landdev.events;
 
 import static net.fexcraft.mod.landdev.util.InteractHandler.control;
-import static net.fexcraft.mod.landdev.util.TranslationUtil.translate;
 
-import net.fexcraft.lib.common.math.Time;
-import net.fexcraft.lib.common.utils.Formatter;
 import net.fexcraft.lib.mc.utils.Print;
-import net.fexcraft.mod.landdev.data.chunk.ChunkType;
-import net.fexcraft.mod.landdev.data.chunk.Chunk_;
-import net.fexcraft.mod.landdev.data.player.LDPlayer;
-import net.fexcraft.mod.landdev.util.LDConfig;
 import net.fexcraft.mod.landdev.util.Protector;
-import net.fexcraft.mod.landdev.util.ResManager;
 import net.minecraft.block.BlockSign;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumHand;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
@@ -32,7 +21,7 @@ public class InteractionEvents {
 	public static void onBlockBreak0(BlockEvent.BreakEvent event){
 		if(event.getWorld().isRemote || event.getPlayer().dimension != 0) return;
 		if(!control(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getPlayer(), false)){
-			Print.bar(event.getPlayer(), translate("interact.break.noperm"));
+			Print.bar(event.getPlayer(), "landdev.interact.break.noperm");
 			event.setCanceled(true);
 		}
 	}
@@ -41,7 +30,7 @@ public class InteractionEvents {
 	public static void onBlockPlace(BlockEvent.PlaceEvent event){
 		if(event.getWorld().isRemote || event.getPlayer().dimension != 0){ return; }
 		if(!control(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getPlayer(), false)){
-			Print.bar(event.getPlayer(), translate("interact.place.noperm"));
+			Print.bar(event.getPlayer(), "landdev.interact.place.noperm");
 			event.setCanceled(true);
 		}
 	}
@@ -52,7 +41,7 @@ public class InteractionEvents {
 		IBlockState state = event.getWorld().getBlockState(event.getPos());
 		boolean check = state.getBlock() instanceof BlockSign == false && Protector.INSTANCE.isProtected(state);
 		if(check && !control(event.getPos().getX(), event.getPos().getY(), event.getPos().getZ(), event.getEntityPlayer(), true)){
-			Print.bar(event.getEntityPlayer(), translate("interact.interact.noperm"));
+			Print.bar(event.getEntityPlayer(), "landdev.interact.interact.noperm");
 			event.setCanceled(true);
 		}
 	}
