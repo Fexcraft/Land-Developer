4bee0af99d1b5e5ee328b387dd6d7c98658bd309
Ferdinand Calo <fexcalo@gmail.com>
2025-08-27 21:40:33 +0200

    Update CntCmd.java

--- forge12/src/main/java/net/fexcraft/mod/landdev/cmd/CntCmd.java
+++ forge12/src/main/java/net/fexcraft/mod/landdev/cmd/CntCmd.java
@@ -1,6 +1,5 @@
 package net.fexcraft.mod.landdev.cmd;
 
-import net.fexcraft.lib.mc.utils.Print;
 import net.fexcraft.mod.landdev.data.chunk.Chunk_;
 import net.fexcraft.mod.landdev.data.county.County;
 import net.fexcraft.mod.landdev.data.player.LDPlayer;
@@ -17,7 +16,6 @@ import net.minecraft.server.MinecraftServer;
 import java.util.List;
 
 import static net.fexcraft.mod.landdev.data.PermAction.CREATE_COUNTY;
-import static net.fexcraft.mod.landdev.util.TranslationUtil.translateCmd;
 
 public class CntCmd extends CommandBase {
 
@@ -55,8 +53,8 @@ public class CntCmd extends CommandBase {
 	    			boolean cn = region.norms.get("new-counties").bool();
 	    			boolean pp = ply.hasPermit(CREATE_COUNTY, region.getLayer(), region.id);
 	    			if(!cn && !pp){
-		    			Print.chat(sender, translateCmd("ct.no_new_county"));
-		    			Print.chat(sender, translateCmd("ct.no_create_permit"));
+		    			ply.entity.send("landdev.cmd.ct.no_new_county");
+						ply.entity.send("landdev.cmd.ct.no_create_permit");
 	    			}
 	    			else{
 						ply.entity.openUI(LDKeys.COUNTY, County.UI_CREATE, 0, 0);
@@ -64,7 +62,7 @@ public class CntCmd extends CommandBase {
 	    			return;
 	    		}
 	    		default:{
-	    			Print.chat(sender, translateCmd("unknown_argument"));
+					ply.entity.send("landdev.cmd.unknown_argument");
 	    			return;
 	    		}
     		}
