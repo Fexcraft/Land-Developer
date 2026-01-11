1670504066144a9af95558e024f7bd79adf4355b
Ferdinand Calo <fexcalo@gmail.com>
2026-01-11 19:19:48 +0100

    Create SpaceDefinitionCache.java

---
+++ common/java/net/fexcraft/mod/landdev/data/player/SpaceDefinitionCache.java
@@ -0,0 +1,32 @@
+package net.fexcraft.mod.landdev.data.player;
+
+import net.fexcraft.lib.common.math.V3I;
+import net.fexcraft.mod.landdev.LandDev;
+import net.fexcraft.mod.uni.tag.TagCW;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class SpaceDefinitionCache {
+
+	public V3I pos;
+	public V3I size;
+
+	public SpaceDefinitionCache(V3I vec){
+		pos = vec.copy();
+		size = new V3I(1, 1, 1);
+	}
+
+	public void start(LDPlayer player){
+		TagCW com = TagCW.create();
+		fillPacket("space_def_start", com);
+		LandDev.sendTo(com, player);
+	}
+
+	private void fillPacket(String task, TagCW com){
+		com.set("task", task);
+		com.set("pos", pos);
+		com.set("size", size);
+	}
+
+}
