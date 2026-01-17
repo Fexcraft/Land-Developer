aa637979b76083c8a49fc79183cfb1e967a2cccf
Ferdinand Calo <fexcalo@gmail.com>
2026-01-17 22:24:09 +0100

    Update SpaceDefUI.java

--- common/java/net/fexcraft/mod/landdev/ui/SpaceDefUI.java
+++ common/java/net/fexcraft/mod/landdev/ui/SpaceDefUI.java
@@ -7,6 +7,8 @@ import net.fexcraft.mod.uni.ui.ContainerInterface;
 import net.fexcraft.mod.uni.ui.UIButton;
 import net.fexcraft.mod.uni.ui.UserInterface;
 
+import static net.fexcraft.mod.landdev.data.prop.ClientPropCache.space;
+
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
@@ -22,12 +24,12 @@ public class SpaceDefUI extends UserInterface {
 	@Override
 	public void predraw(float ticks, int mx, int my){
 		if(!con.refresh) return;
-		fields.get("pos_x").text(con.pos.x);
-		fields.get("pos_y").text(con.pos.y);
-		fields.get("pos_z").text(con.pos.z);
-		fields.get("size_x").text(con.size.x);
-		fields.get("size_y").text(con.size.y);
-		fields.get("size_z").text(con.size.z);
+		fields.get("pos_x").text(space.pos.x);
+		fields.get("pos_y").text(space.pos.y);
+		fields.get("pos_z").text(space.pos.z);
+		fields.get("size_x").text(space.size.x);
+		fields.get("size_y").text(space.size.y);
+		fields.get("size_z").text(space.size.z);
 		con.refresh = false;
 	}
 
@@ -66,51 +68,51 @@ public class SpaceDefUI extends UserInterface {
 				break;
 			}
 			case "x+":{
-				com.set("x", con.pos.x + 1);
+				com.set("x", space.pos.x + 1);
 				break;
 			}
 			case "x-":{
-				com.set("x", con.pos.x - 1);
+				com.set("x", space.pos.x - 1);
 				break;
 			}
 			case "y+":{
-				com.set("y", con.pos.y + 1);
+				com.set("y", space.pos.y + 1);
 				break;
 			}
 			case "y-":{
-				com.set("y", con.pos.y - 1);
+				com.set("y", space.pos.y - 1);
 				break;
 			}
 			case "z+":{
-				com.set("z", con.pos.z + 1);
+				com.set("z", space.pos.z + 1);
 				break;
 			}
 			case "z-":{
-				com.set("z", con.pos.z - 1);
+				com.set("z", space.pos.z - 1);
 				break;
 			}
 			case "w+":{
-				com.set("w", con.size.x + 1);
+				com.set("w", space.size.x + 1);
 				break;
 			}
 			case "w-":{
-				com.set("w", con.size.x - 1);
+				com.set("w", space.size.x - 1);
 				break;
 			}
 			case "h+":{
-				com.set("h", con.size.y + 1);
+				com.set("h", space.size.y + 1);
 				break;
 			}
 			case "h-":{
-				com.set("h", con.size.y - 1);
+				com.set("h", space.size.y - 1);
 				break;
 			}
 			case "d+":{
-				com.set("d", con.size.z + 1);
+				com.set("d", space.size.z + 1);
 				break;
 			}
 			case "d-":{
-				com.set("d", con.size.z - 1);
+				com.set("d", space.size.z - 1);
 				break;
 			}
 		}
