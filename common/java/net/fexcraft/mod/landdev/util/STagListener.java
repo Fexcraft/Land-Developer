9ca5371b56b920d06215adcc6060bc7307e2d072
Ferdinand Calo <fexcalo@gmail.com>
2026-01-17 19:21:37 +0100

    Update STagListener.java

---
+++ common/java/net/fexcraft/mod/landdev/util/STagListener.java
@@ -0,0 +1,29 @@
+package net.fexcraft.mod.landdev.util;
+
+import net.fexcraft.mod.landdev.LandDev;
+import net.fexcraft.mod.uni.packet.PacketTagListener;
+import net.fexcraft.mod.uni.tag.TagCW;
+import net.fexcraft.mod.uni.world.EntityW;
+
+import java.util.HashMap;
+import java.util.function.BiConsumer;
+
+/**
+ * @author Ferdinand Calo' (FEX___96)
+ */
+public class STagListener implements PacketTagListener {
+
+	public static HashMap<String, BiConsumer<TagCW, EntityW>> TASKS = new HashMap<>();
+
+	@Override
+	public void handle(TagCW packet, EntityW player){
+		if(!packet.has("task")) return;
+		BiConsumer<TagCW, EntityW> cons = TASKS.get(packet.getString("task"));
+		if(cons != null) cons.accept(packet, player);
+		else{
+			LandDev.log("Received packet with unknown task '" + packet.getString("task") + "'.");
+			LandDev.log(packet);
+		}
+	}
+
+}
