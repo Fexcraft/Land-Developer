9aabb1ca4c8b4d2652e68e8c42067c15860f56a2
Ferdinand Calo <fexcalo@gmail.com>
2026-01-17 19:22:08 +0100

    Update LandDev.java

--- forge12/src/main/java/net/fexcraft/mod/landdev/LandDev.java
+++ forge12/src/main/java/net/fexcraft/mod/landdev/LandDev.java
@@ -1,17 +1,24 @@
 package net.fexcraft.mod.landdev;
 
+import net.fexcraft.lib.common.math.Time;
 import net.fexcraft.lib.mc.network.PacketHandler;
-import net.fexcraft.mod.fcl.UniFCL;
+import net.fexcraft.lib.mc.render.ExternalTextureHelper;
 import net.fexcraft.mod.landdev.cmd.*;
 import net.fexcraft.mod.landdev.data.player.LDPlayer;
 import net.fexcraft.mod.landdev.events.LocationUpdate;
+import net.fexcraft.mod.landdev.gui.LDGuiImgPreview;
 import net.fexcraft.mod.landdev.util.GuiHandler;
 import net.fexcraft.mod.landdev.util.*;
 import net.fexcraft.mod.uni.EnvInfo;
 import net.fexcraft.mod.uni.impl.PacketTagHandler.I12_PacketTag;
 import net.fexcraft.mod.uni.tag.TagCW;
 import net.fexcraft.mod.uni.world.EntityW;
-import net.minecraft.world.World;
+import net.minecraft.client.Minecraft;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.event.ClickEvent;
+import net.minecraft.util.text.event.HoverEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
@@ -22,6 +29,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 
+import static net.fexcraft.lib.common.utils.Formatter.format;
 import static net.fexcraft.mod.landdev.LDN.PKT_RECEIVER_ID;
 
 @Mod(modid = LDN.MODID, name = LandDev.NAME, version = LandDev.VERSION,
@@ -45,11 +53,44 @@ public class LandDev {
     @EventHandler
     public void init(FMLInitializationEvent event){
         NetworkRegistry.INSTANCE.registerGuiHandler(this, new GuiHandler());
+		LDN.init(this);
         if(event.getSide().isClient()){
-			UniFCL.regTagPacketListener(PKT_RECEIVER_ID, true, new PacketReceiver());
+			LDN.client_init(this);
+			CTagListener.TASKS.put("location_update", (packet, player) -> {
+				int time = packet.has("time") ? packet.getInteger("time") : 10;
+				LocationUpdate.clear(Time.getDate() + (time * 1000));
+				LocationUpdate.loadIcons(packet.getList("icons").local());
+				LocationUpdate.loadLines(packet.getList("lines").local());
+			});
+			CTagListener.TASKS.put("chat_message", (packet, player) -> {
+				NBTTagList list = packet.getList("msg").local();
+				String c = list.tagCount() > 3 ? list.getStringTagAt(3) : "&a";
+				ITextComponent text = null;
+				switch(list.getStringTagAt(0)){
+					case "chat_img":
+						text = new TextComponentString(format(list.getStringTagAt(1)));
+						TextComponentString text1 = new TextComponentString(format(" &a[ &6View &a]"));
+						text1.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.RUN_COMMAND, "/landdev img " + list.getStringTagAt(2) + " " + c + " " + list.getStringTagAt(4)));
+						text1.getStyle().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentString(format(list.getStringTagAt(1)))));
+						text.appendSibling(text1);
+						TextComponentString text2 = new TextComponentString(format(" &e[ &6Open &e]"));
+						text2.getStyle().setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, list.getStringTagAt(2)));
+						text2.getStyle().setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new TextComponentString(format(list.getStringTagAt(1)))));
+						text.appendSibling(text2);
+						break;
+					case "chat":
+					default:
+						text = new TextComponentString(format(LDConfig.CHAT_OVERRIDE_LANG, c, list.getStringTagAt(1), list.getStringTagAt(2)));
+						//text = new TextComponentString(list.toString());
+						break;
+				}
+				Minecraft.getMinecraft().ingameGUI.getChatGUI().printChatMessage(text);
+			});
+			CTagListener.TASKS.put("img_preview_url", (packet, player) -> {
+				LDGuiImgPreview.IMG_URL = ExternalTextureHelper.get(packet.getString("url"));
+			});
         	MinecraftForge.EVENT_BUS.register(new LocationUpdate());
         }
-		LDN.init(this);
     }
 
     @EventHandler
