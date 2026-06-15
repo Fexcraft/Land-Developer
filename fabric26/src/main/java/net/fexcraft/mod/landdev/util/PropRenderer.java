b242ebf100924442fbd0c3d6a3b96ce3a7e75042
Ferdinand Calo <fexcalo@gmail.com>
2026-06-15 21:29:18 +0200

    Update PropRenderer.java

--- fabric26/src/main/java/net/fexcraft/mod/landdev/util/PropRenderer.java
+++ fabric26/src/main/java/net/fexcraft/mod/landdev/util/PropRenderer.java
@@ -8,6 +8,8 @@ import net.fexcraft.mod.landdev.data.prop.ClientPropCache;
 import net.fexcraft.mod.uni.IDL;
 import net.fexcraft.mod.uni.IDLManager;
 import net.minecraft.client.Camera;
+import net.minecraft.client.renderer.SubmitNodeCollector;
+import net.minecraft.util.LightCoordsUtil;
 
 /**
  * @author Ferdinand Calo' (FEX___96)
@@ -16,6 +18,17 @@ public class PropRenderer {
 
 	private static IDL loc0 = IDLManager.getIDLCached("minecraft:textures/block/glowstone.png");
 	private static IDL loc1 = IDLManager.getIDLCached("minecraft:textures/block/diamond_block.png");
+	public static SubmitNodeCollector noco;
+	static {
+		ClientPropCache.render = poly -> {
+			PropRenderer.noco.submitCustomGeometry(Renderer26.stack, Renderer26.type, (pose, cons) -> {
+				Renderer26.pose = pose;
+				Renderer26.cons = cons;
+				Renderer26.light = LightCoordsUtil.FULL_BRIGHT;
+				Renderer26.RENDERER.render(poly);
+			});
+		};
+	}
 
 	public static void renderProps(LevelRenderContext event){
 		if(!ClientPropCache.visible && ClientPropCache.space == null) return;
@@ -23,10 +36,9 @@ public class PropRenderer {
 		double cx = camera.position().x;
 		double cy = camera.position().y;
 		double cz = camera.position().z;
+		noco = event.submitNodeCollector();
 		PoseStack pose = event.poseStack();
 		Renderer26.stack = pose;
-		Renderer26.noco = event.submitNodeCollector();
-		Renderer26.light = 255;
 		pose.pushPose();
 		pose.translate(-cx, -cy, -cz);
 		if(ClientPropCache.space != null){
