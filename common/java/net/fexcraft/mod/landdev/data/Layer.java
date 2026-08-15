303b49652c76324af5372ed39fa1f1ca3a72f994
Ferdinand Calo <fexcalo@gmail.com>
2026-08-15 11:02:28 +0200

    Update Layer.java

--- common/java/net/fexcraft/mod/landdev/data/Layer.java
+++ common/java/net/fexcraft/mod/landdev/data/Layer.java
@@ -1,5 +1,10 @@
 package net.fexcraft.mod.landdev.data;
 
+import net.fexcraft.mod.landdev.data.county.County;
+import net.fexcraft.mod.landdev.data.district.District;
+import net.fexcraft.mod.landdev.data.municipality.Municipality;
+import net.fexcraft.mod.landdev.data.region.Region;
+
 /**
  * @author Ferdinand Calo' (FEX___96)
  */
@@ -15,6 +20,18 @@ public interface Layer {
 
 	public default int lid(){
 		return 0;
+	}
+
+	public default Manageable manageable(){
+		switch(getLayer()){
+			case COMPANY:
+				break;
+			case DISTRICT: return ((District)this).manage;
+			case MUNICIPALITY: return ((Municipality)this).manage;
+			case COUNTY: return ((County)this).manage;
+			case REGION: return ((Region)this).manage;
+		}
+		return null;
 	}
 
 }
